c6970ece459c9a244cbe2a8073f4a0e39144f17f
user <fulminazzo21@gmail.com>
2026-01-11 16:09:56 +0100

    Added tests for parseProperties

---
+++ src/test/groovy/it/fulminazzo/conveyor/artifact/pom/PomBuilderTest.groovy
@@ -0,0 +1,38 @@
+package it.fulminazzo.conveyor.artifact.pom
+
+import spock.lang.Specification
+
+class PomBuilderTest extends Specification {
+
+    def 'test that parseProperties returns correct properties'() {
+        given:
+        def builder = newBuilder("""
+            <hello>world</hello>
+            <dependency.version>1.0</dependency.version>
+            <dependency.name>conveyor</dependency.name>
+            <name>\${dependency.name}</name>
+        """)
+        builder.reader.next()
+
+        when:
+        builder.parseProperties()
+
+        and:
+        def field = PomBuilder.getDeclaredField('properties')
+        field.accessible = true
+        def properties = field.get(builder)
+
+        then:
+        properties == [
+                'hello': 'world',
+                'dependency.version': '1.0',
+                'dependency.name': 'conveyor',
+                'name': '${dependency.name}'
+        ]
+    }
+
+    private static PomBuilder newBuilder(final String data) {
+        return new PomBuilder(new ByteArrayInputStream("""<project>$data</project>""".bytes))
+    }
+
+}
