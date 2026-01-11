2bcb78af9c155be9ed9b2e372eeac9c57f79d61e
user <fulminazzo21@gmail.com>
2026-01-11 11:20:08 +0100

    Added tests for resolve method

--- src/test/groovy/it/fulminazzo/conveyor/downloader/DownloaderImplTest.groovy
+++ src/test/groovy/it/fulminazzo/conveyor/downloader/DownloaderImplTest.groovy
@@ -10,6 +10,64 @@ class DownloaderImplTest extends Specification {
         this.downloader = new DownloaderImpl(file)
     }
 
+    def 'test that resolveToFile is able to download #path'() {
+        given:
+        def workDir = this.downloader.workingDir
+        if (workDir.isDirectory()) workDir.deleteDir()
+
+        and:
+        this.downloader.addBaseUrls(
+                'invalidoffline.com',
+                'repo.maven.apache.org/maven2'
+        )
+
+        and:
+        def expected = new File(this.downloader.workingDir, path)
+
+        when:
+        this.downloader.resolveToFile(path)
+
+        then:
+        expected.exists()
+
+        and:
+        expected.size() > 0
+
+        where:
+        path << [
+                'org/projectlombok/lombok/1.18.42/lombok-1.18.42.pom',
+                '/org/projectlombok/lombok/1.18.42/lombok-1.18.42.pom'
+        ]
+    }
+
+    def 'test that resolveToFile throws on download error'() {
+        given:
+        def workDir = this.downloader.workingDir
+        if (workDir.isDirectory()) workDir.deleteDir()
+
+        and:
+        this.downloader.addBaseUrls(
+                'invalidoffline.com',
+                'invalidoffline2.com'
+        )
+
+        when:
+        this.downloader.resolveToFile('path')
+
+        then:
+        def e = thrown(DownloadException)
+        e.message.contains("'path'")
+    }
+
+    def 'test that resolve throws on missing URLs'() {
+        when:
+        this.downloader.resolve('path')
+
+        then:
+        def e = thrown(DownloadException)
+        e.message.contains('No base URL')
+    }
+
     def 'test that addBaseUrls adds URLs in correct format'() {
         given:
         def urls = [
