cb2ee3eeb3f71c6504f7ea225f4c3f0caeaae839
user <fulminazzo21@gmail.com>
2026-01-11 16:22:57 +0100

    Added tests for parseRepository method

--- src/test/groovy/it/fulminazzo/conveyor/artifact/pom/PomBuilderTest.groovy
+++ src/test/groovy/it/fulminazzo/conveyor/artifact/pom/PomBuilderTest.groovy
@@ -1,5 +1,8 @@
 package it.fulminazzo.conveyor.artifact.pom
 
+import it.fulminazzo.conveyor.artifact.pom.repository.ChecksumPolicy
+import it.fulminazzo.conveyor.artifact.pom.repository.Repository
+import it.fulminazzo.conveyor.artifact.pom.repository.update.UpdatePolicy
 import spock.lang.Specification
 
 class PomBuilderTest extends Specification {
@@ -26,11 +29,60 @@ class PomBuilderTest extends Specification {
 
         then:
         properties == [
-                'hello': 'world',
+                'hello'             : 'world',
                 'dependency.version': '1.0',
-                'dependency.name': 'conveyor',
-                'name': '${dependency.name}'
+                'dependency.name'   : 'conveyor',
+                'name'              : '${dependency.name}'
         ]
+    }
+
+    def 'test that parseRepository returns correct repository'() {
+        given:
+        def builder = newBuilder("""
+            <repository>
+                <id>id</id>
+                <name>name</name>
+                <url>url</url>
+                
+                <layout>default</layout>
+            
+                <releases>
+                    <enabled>true</enabled>
+                    <updatePolicy>daily</updatePolicy>
+                    <checksumPolicy>warn</checksumPolicy>
+                </releases>
+            
+                <snapshots>
+                    <enabled>true</enabled>
+                    <updatePolicy>always</updatePolicy>
+                    <checksumPolicy>fail</checksumPolicy>
+                </snapshots>
+            </repository>
+        """)
+        builder.reader.next()
+
+        and:
+        def expected = Repository.builder()
+                .id('id')
+                .name('name')
+                .url('url')
+                .releases(Repository.Policy.builder()
+                        .enabled(true)
+                        .updatePolicy(UpdatePolicy.of('daily'))
+                        .checksumPolicy(ChecksumPolicy.WARN)
+                        .build())
+                .snapshots(Repository.Policy.builder()
+                        .enabled(true)
+                        .updatePolicy(UpdatePolicy.of('always'))
+                        .checksumPolicy(ChecksumPolicy.FAIL)
+                        .build())
+                .build()
+
+        when:
+        def repository = builder.parseRepository()
+
+        then:
+        repository == expected
     }
 
     private static PomBuilder newBuilder(final String data) {
