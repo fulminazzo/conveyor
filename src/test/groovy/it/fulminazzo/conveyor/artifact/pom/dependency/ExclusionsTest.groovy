b042e610d16e5cd714c0187badc816ce215c9d06
user <fulminazzo21@gmail.com>
2026-01-11 15:48:38 +0100

    Added tests for Exclusions

---
+++ src/test/groovy/it/fulminazzo/conveyor/artifact/pom/dependency/ExclusionsTest.groovy
@@ -0,0 +1,34 @@
+package it.fulminazzo.conveyor.artifact.pom.dependency
+
+import spock.lang.Specification
+
+class ExclusionsTest extends Specification {
+
+    def 'test that isExcluded returns #expected for #groupId and #artifactId (wildcard: #wildcard)'() {
+        given:
+        def exclusions = new Exclusions()
+
+        and:
+        exclusions.add('it.fulminazzo', 'first')
+        exclusions.add('it.fulminazzo', 'second')
+        exclusions.add('it.wildcard', '*')
+        exclusions.add('*', 'wildcard')
+        if (wildcard) exclusions.add('*', '*')
+
+        when:
+        def actual = exclusions.isExcluded(groupId, artifactId)
+
+        then:
+        actual == expected
+
+        where:
+        groupId         | artifactId | wildcard || expected
+        'it.fulminazzo' | 'first'    | false    || true
+        'it.fulminazzo' | 'second'   | false    || true
+        'it.fulminazzo' | 'third'    | false    || false
+        'it.wildcard'   | 'first'    | false    || true
+        '*'             | 'wildcard' | false    || true
+        'it.fulminazzo' | 'third'    | true     || true
+    }
+
+}
