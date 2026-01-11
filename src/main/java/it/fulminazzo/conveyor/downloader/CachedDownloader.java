f5ae7407273043f2377ebeedea79ade347b99772
user <fulminazzo21@gmail.com>
2026-01-11 11:59:23 +0100

    Fixed resolveChecksum not correctly returning checksum

--- src/main/java/it/fulminazzo/conveyor/downloader/CachedDownloader.java
+++ src/main/java/it/fulminazzo/conveyor/downloader/CachedDownloader.java
@@ -57,7 +57,7 @@ final class CachedDownloader extends DownloaderImpl {
                                            final @NotNull ChecksumAlgorithm algorithm) throws DownloadException {
         String finalPath = resourcePath + "." + algorithm.getExtension();
         try (InputStream stream = resolve(finalPath)) {
-            return stream.toString();
+            return new String(stream.readAllBytes());
         } catch (IOException e) {
             throw new DownloadException(String.format("Error while downloading resource '%s'", finalPath), e);
         }
