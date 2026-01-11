bd8595dffb5d4507db24ffc073f898a8c3fd725e
user <fulminazzo21@gmail.com>
2026-01-11 15:10:54 +0100

    Created UpdatePolicy interface with implementations to represent various updates policies

---
+++ src/main/java/it/fulminiazzo/conveyor/artifact/pom/repository/update/IntervalUpdatePolicy.java
@@ -0,0 +1,29 @@
+package it.fulminiazzo.conveyor.artifact.pom.repository.update;
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+/**
+ * Represents the <code>interval</code> update policy.
+ */
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
+final class IntervalUpdatePolicy implements UpdatePolicy {
+    private final long interval;
+
+    @Override
+    public boolean shouldUpdate(final long lastUpdate) {
+        LocalDateTime time = Instant.ofEpochMilli(lastUpdate)
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+        LocalDateTime now = LocalDateTime.now();
+        return time.plusMinutes(this.interval).isBefore(now);
+    }
+
+}
---
+++ src/main/java/it/fulminiazzo/conveyor/artifact/pom/repository/update/UpdatePolicy.java
@@ -0,0 +1,46 @@
+package it.fulminiazzo.conveyor.artifact.pom.repository.update;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents the update policy of a repository.
+ */
+public interface UpdatePolicy {
+
+    /**
+     * Checks if the current policy allows updating, given the last update time.
+     *
+     * @param lastUpdate the last update
+     * @return true if it does
+     */
+    boolean shouldUpdate(final long lastUpdate);
+
+    /**
+     * Gets the corresponding update policy of the given raw policy.
+     *
+     * @param rawPolicy the raw policy
+     * @return the update policy
+     */
+    static @NotNull UpdatePolicy of(final @NotNull String rawPolicy) {
+        for (UpdatePolicyEnum updatePolicy : UpdatePolicyEnum.values())
+            if (updatePolicy.name().toLowerCase().equals(rawPolicy))
+                return updatePolicy;
+        if (rawPolicy.startsWith("interval")) {
+            String time = rawPolicy.substring("interval".length());
+            if (time.startsWith(":")) {
+                time = time.substring(1);
+                try {
+                    long actualTime = Long.parseLong(time);
+                    return new IntervalUpdatePolicy(actualTime);
+                } catch (NumberFormatException ignored) {
+                }
+            }
+            throw new IllegalArgumentException(
+                    String.format("Could not determine time interval from update policy '%s'. ", rawPolicy) +
+                    "Format: interval:<minutes>"
+            );
+        }
+        throw new IllegalArgumentException(String.format("Could not find matching update policy of '%s'", rawPolicy));
+    }
+
+}
