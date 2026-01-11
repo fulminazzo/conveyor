e90f8672a1ccbd0bb9aad7b1ee6b006a80c4b497
user <fulminazzo21@gmail.com>
2026-01-11 16:05:12 +0100

    Added parseProperties method

--- src/main/java/it/fulminazzo/conveyor/artifact/pom/PomBuilder.java
+++ src/main/java/it/fulminazzo/conveyor/artifact/pom/PomBuilder.java
@@ -3,25 +3,50 @@ package it.fulminazzo.conveyor.artifact.pom;
 import org.jetbrains.annotations.NotNull;
 
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Responsible for creating a {@link Pom} object.
  */
 final class PomBuilder {
+    private final @NotNull Map<String, String> properties = new HashMap<>();
+
     private final @NotNull XMLStreamReader reader;
 
     /**
      * Instantiates a new Pom builder.
      *
      * @param inputStream the input stream
-     * @throws XMLStreamException in case of reading errors
+     * @throws XMLStreamException in case of reading or parsing errors
      */
     public PomBuilder(final @NotNull InputStream inputStream) throws XMLStreamException {
         XMLInputFactory factory = XMLInputFactory.newInstance();
         this.reader = factory.createXMLStreamReader(inputStream);
+    }
+
+    /**
+     * Handles the <b>&lt;properties&gt;</b> tag in the document.
+     *
+     * @throws XMLStreamException in case of reading or parsing errors
+     */
+    void parseProperties() throws XMLStreamException {
+        while (this.reader.hasNext())
+            switch (this.reader.next()) {
+                case XMLStreamConstants.START_ELEMENT: {
+                    String key = this.reader.getLocalName();
+                    String value = this.reader.getElementText();
+                    this.properties.put(key, value);
+                    break;
+                }
+                case XMLStreamConstants.END_ELEMENT: {
+                    return;
+                }
+            }
     }
 
 }
