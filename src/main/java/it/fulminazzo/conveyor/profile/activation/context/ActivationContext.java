da91e6c0ad4cc114d42b4317dc2ef2d7b6a2eb52
user <fulminazzo21@gmail.com>
2026-01-12 11:50:54 +0100

    Created ActivationContext

---
+++ src/main/java/it/fulminazzo/conveyor/profile/activation/context/ActivationContext.java
@@ -0,0 +1,8 @@
+package it.fulminazzo.conveyor.profile.activation.context;
+
+/**
+ * Represents the current context where an {@link it.fulminazzo.conveyor.profile.activation.Activation} is checked.
+ */
+public interface ActivationContext {
+
+}
