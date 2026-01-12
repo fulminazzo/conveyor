afa302b0609f7d8d8e835fbaad9dbda30f7aee4e
user <fulminazzo21@gmail.com>
2026-01-12 13:50:46 +0100

    Implemented range parsing in isEnabled

--- src/main/java/it/fulminazzo/conveyor/profile/activation/JdkActivation.java
+++ src/main/java/it/fulminazzo/conveyor/profile/activation/JdkActivation.java
@@ -3,6 +3,9 @@ package it.fulminazzo.conveyor.profile.activation;
 import it.fulminazzo.conveyor.profile.activation.context.ActivationContext;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * An {@link Activation} that will check the context JDK version.
  *
@@ -17,11 +20,48 @@ record JdkActivation(@NotNull String jdk) implements Activation {
         boolean negated = jdk.startsWith(NEGATION);
         if (negated) jdk = jdk.substring(1);
 
-        if (jdk.startsWith("[") || jdk.startsWith("("))
-            throw new UnsupportedOperationException("Ranges");
+        if (jdk.startsWith("[") || jdk.startsWith("(")) {
+            Matcher matcher = Pattern.compile("([\\[(])([^,]*),([^])]*)([])])").matcher(jdk);
+            if (matcher.find()) {
+                boolean leftInclusive = matcher.group(1).equals("[");
+                String leftVersion = matcher.group(2);
+                int left = leftVersion.isEmpty() ? 1 : compareVersion(current, leftVersion);
+
+                if (left < 0 || (!leftInclusive && left == 0)) return false;
+
+                boolean rightInclusive = matcher.group(4).equals("]");
+                String rightVersion = matcher.group(3);
+                int right = rightVersion.isEmpty() ? -1 : compareVersion(current, rightVersion);
+
+                return right <= 0 && (rightInclusive || right != 0);
+            } else throw new IllegalArgumentException(String.format("Invalid range '%s'", this.jdk));
+        }
 
         if (current.startsWith(jdk)) return !negated;
         else return negated;
+    }
+
+    private int compareVersion(final @NotNull String version1,
+                               final @NotNull String version2) {
+        final String regex = "[._\\-+]";
+        String[] split1 = version1.split(regex);
+        String[] split2 = version2.split(regex);
+
+        int length = Math.max(split1.length, split2.length);
+        for (int i = 0; i < length; i++) {
+            int n1 = i < split1.length ? parseToken(split1[i]) : 0;
+            int n2 = i < split2.length ? parseToken(split2[i]) : 0;
+            if (n1 != n2) return Integer.compare(n1, n2);
+        }
+        return 0;
+    }
+
+    private int parseToken(final @NotNull String token) {
+        try {
+            return Integer.parseInt(token.replaceAll("\\D", ""));
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 
 }
