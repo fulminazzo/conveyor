3cb6fb8dece4a2607a2fcf5f35b14d4ac11370c8
user <fulminazzo21@gmail.com>
2026-01-12 12:46:04 +0100

    Optimized code

--- src/main/java/it/fulminazzo/conveyor/profile/activation/PropertyActivation.java
+++ src/main/java/it/fulminazzo/conveyor/profile/activation/PropertyActivation.java
@@ -16,28 +16,23 @@ record PropertyActivation(@NotNull String name, @Nullable String value) implemen
     @Override
     public boolean isEnabled(final @NotNull ActivationContext context) {
         String actualName = this.name;
-        boolean matchName = true;
-        if (actualName.startsWith(NEGATION)) {
-            actualName = actualName.substring(1);
-            matchName = false;
-        }
+        boolean negatedName = actualName.startsWith(NEGATION);
+        if (negatedName) actualName = actualName.substring(1);
 
         @Nullable String actualValue = this.value;
-        boolean matchValue = true;
+        final boolean negatedValue;
         if (actualValue != null) {
-            if (actualName.startsWith(NEGATION)) {
-                actualValue = actualValue.substring(1);
-                matchValue = false;
-            }
-        }
+            negatedValue = actualName.startsWith(NEGATION);
+            if (negatedValue) actualValue = actualValue.substring(1);
+        } else negatedValue = false;
 
         String propertyValue = actualName.equals(packaging) ? context.getPackaging() : context.getProperty(actualName);
-        if (propertyValue == null) return !matchName;
+        if (propertyValue == null) return negatedName;
         if (actualValue == null) return true;
 
         boolean matches = actualValue.equals(propertyValue);
-        if (matchValue) return matches;
-        else return !matches;
+        if (negatedValue) return !matches;
+        else return matches;
     }
 
 }
