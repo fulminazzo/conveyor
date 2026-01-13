c759bd161a2f7388385c7c1f0f8ab8b7df04053d
user <fulminazzo21@gmail.com>
2026-01-13 15:40:35 +0100

    Added builder function

--- src/main/java/it/fulminazzo/conveyor/model/profile/activation/Activation.java
+++ src/main/java/it/fulminazzo/conveyor/model/profile/activation/Activation.java
@@ -1,6 +1,7 @@
 package it.fulminazzo.conveyor.model.profile.activation;
 
 import it.fulminazzo.conveyor.model.profile.activation.context.ActivationContext;
+import it.fulminazzo.conveyor.xml.XmlParser;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -16,5 +17,15 @@ public interface Activation {
      * @return true if it is
      */
     boolean isEnabled(final @NotNull ActivationContext context);
+
+    /**
+     * Instantiates a new builder to create a {@link Activation} object.
+     *
+     * @param xmlParser the XML parser
+     * @return the builder
+     */
+    static @NotNull ActivationBuilder builder(final @NotNull XmlParser xmlParser) {
+       return new ActivationBuilder(xmlParser);
+    }
 
 }
