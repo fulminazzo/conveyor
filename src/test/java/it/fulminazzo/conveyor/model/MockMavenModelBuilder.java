625226beae22e65ef3aba09dc9526169c682ef85
user <fulminazzo21@gmail.com>
2026-01-13 01:40:50 +0100

    Moved MockMavenModelBuilder to model package and updated with new MavenModelBuilder directives

---
+++ src/test/java/it/fulminazzo/conveyor/model/MockMavenModelBuilder.java
@@ -0,0 +1,32 @@
+package it.fulminazzo.conveyor.model;
+
+import it.fulminazzo.conveyor.xml.XmlParser;
+import it.fulminazzo.conveyor.xml.XmlParserException;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+final class MockMavenModelBuilder extends MavenModelBuilder<Object> {
+
+    public MockMavenModelBuilder(final @NotNull XmlParser parser) {
+        super(parser);
+    }
+
+    @Override
+    public Object build() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static @NotNull MavenModelBuilder<?> newBuilder(final @NotNull String rawData) throws BuilderException {
+        try {
+            InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.UTF_8));
+            XmlParser parser = XmlParser.newParser(inputStream);
+            return new MockMavenModelBuilder(parser);
+        } catch (XmlParserException e) {
+            throw new BuilderException(e);
+        }
+    }
+
+}
