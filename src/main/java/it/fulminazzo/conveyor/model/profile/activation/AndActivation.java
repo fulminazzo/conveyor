5a0b43824e65d2369ab40e79df76ac3f83416d0f
user <fulminazzo21@gmail.com>
2026-01-13 13:56:36 +0100

    Created AndActivation to keep track of all the activations

---
+++ src/main/java/it/fulminazzo/conveyor/model/profile/activation/AndActivation.java
@@ -0,0 +1,31 @@
+package it.fulminazzo.conveyor.model.profile.activation;
+
+import it.fulminazzo.conveyor.model.profile.activation.context.ActivationContext;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A special type of {@link Activation} that will check for all
+ * the given {@link Activation}s in its {@link #isEnabled(ActivationContext)} method.
+ */
+final class AndActivation implements Activation {
+    private final @NotNull Map<String, Activation> activations = new HashMap<>();
+
+    @Override
+    public boolean isEnabled(final @NotNull ActivationContext context) {
+        return this.activations.values().stream().allMatch(a -> a.isEnabled(context));
+    }
+
+    /**
+     * Adds a new {@link Activation}.
+     *
+     * @param id         the identifier of the activation. Must be unique (or will be overridden)
+     * @param activation the activation
+     */
+    public void addActivation(final @NotNull String id, final @NotNull Activation activation) {
+        this.activations.put(id, activation);
+    }
+
+}
