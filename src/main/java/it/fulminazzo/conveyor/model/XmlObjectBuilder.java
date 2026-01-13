e5f1faca90672baa523f639db9f0e52c91f09664
user <fulminazzo21@gmail.com>
2026-01-13 01:18:29 +0100

    Created XmlObjectBuilder Moved buildObject method from MavenModelBuilder

---
+++ src/main/java/it/fulminazzo/conveyor/model/XmlObjectBuilder.java
@@ -0,0 +1,45 @@
+package it.fulminazzo.conveyor.model;
+
+import it.fulminazzo.conveyor.xml.XmlParser;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
+
+/**
+ * A special type of builder that uses <b>XML</b> documents to create objects.
+ *
+ * @param <O> the type of the built object
+ */
+@RequiredArgsConstructor
+public abstract class XmlObjectBuilder<O> {
+    protected final @NotNull XmlParser parser;
+
+    /**
+     * Builds the object.
+     *
+     * @return the object
+     */
+    public abstract O build();
+
+    /**
+     * Wrapper for building new objects from builders.
+     * Replaces any building errors with a {@link BuilderException}.
+     *
+     * @param <T>           the type of the built object
+     * @param name          the name of the building object
+     * @param buildFunction the build function
+     * @return the built object
+     * @throws BuilderException in case of building errors
+     */
+    protected <T> @NotNull T buildObject(final @NotNull String name,
+                                         final @NotNull Supplier<T> buildFunction
+    ) throws BuilderException {
+        try {
+            return buildFunction.get();
+        } catch (RuntimeException e) {
+            throw new BuilderException(String.format("Could not build %s", name), e);
+        }
+    }
+
+}
