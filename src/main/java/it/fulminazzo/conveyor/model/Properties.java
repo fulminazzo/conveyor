b71995de0861aec9e848a57202bbb75972c6ee7c
user <fulminazzo21@gmail.com>
2026-01-14 16:25:16 +0100

    Updated Properties to allow override of properties

--- src/main/java/it/fulminazzo/conveyor/model/Properties.java
+++ src/main/java/it/fulminazzo/conveyor/model/Properties.java
@@ -56,13 +56,12 @@ public final class Properties implements Map<String, String> {
 
     @Override
     public @Nullable String put(String s, String s2) {
-        if (!containsKey(s)) return this.delegate.put(s, s2);
-        else return get(s);
+        return this.delegate.put(s, s2);
     }
 
     @Override
     public void putAll(@NotNull Map<? extends String, ? extends String> map) {
-        map.forEach(this::put);
+        this.delegate.putAll(map);
     }
 
     @Override
--- src/test/groovy/it/fulminazzo/conveyor/model/PropertiesTest.groovy
+++ src/test/groovy/it/fulminazzo/conveyor/model/PropertiesTest.groovy
@@ -20,28 +20,4 @@ class PropertiesTest extends Specification {
         actual == 'Hello, world! ${unknown}'
     }
 
-    def 'test that properties does not override on put and putAll'() {
-        given:
-        def properties = new Properties()
-
-        and:
-        properties.putAll([a: '1', b: '2', c: '3'])
-
-        when:
-        properties.putAll([a: '4', b: '5', c: '6', d: '7'])
-
-        and:
-        properties << [a: '8', b: '9', c: '10', d: '11', e: '12']
-
-        and:
-        for (def i in 12..17) {
-            char c = (char) 'a'
-            c += i - 12
-            properties.put(String.valueOf(c), String.valueOf(i))
-        }
-
-        then:
-        properties == [a: '1', b: '2', c: '3', d: '7', e: '12', f: '17']
-    }
-
 }
