7139535620be014aa71ceeb80ef49be4099bce17
user <fulminazzo21@gmail.com>
2026-01-14 14:32:16 +0100

    Added activeProfiles field with populateActiveProfiles method

--- src/main/java/it/fulminazzo/conveyor/model/pom/EffectivePomBuilder.java
+++ src/main/java/it/fulminazzo/conveyor/model/pom/EffectivePomBuilder.java
@@ -1,7 +1,12 @@
 package it.fulminazzo.conveyor.model.pom;
 
+import it.fulminazzo.conveyor.model.profile.Profile;
+import it.fulminazzo.conveyor.model.profile.activation.context.ActivationContext;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Responsible for creating a {@link EffectivePom} object.
@@ -10,5 +15,20 @@ import org.jetbrains.annotations.NotNull;
 final class EffectivePomBuilder {
     private final @NotNull Pom startingPom;
     private final @NotNull PomResolver pomResolver;
+
+    private final @NotNull Set<Profile> activeProfiles = new HashSet<>();
+
+    /**
+     * Loads all the profiles of the given {@link #startingPom}
+     * whose conditions are met under the given context.
+     *
+     * @param context the context
+     */
+    void populateActiveProfiles(final @NotNull ActivationContext context) {
+        this.activeProfiles.clear();
+        this.startingPom.getProfiles().stream()
+                .filter(p -> p.getActivation().isEnabled(context))
+                .forEach(this.activeProfiles::add);
+    }
 
 }
