d0808993a1edf79814563a8db6860f85ddc29a12
user <fulminazzo21@gmail.com>
2026-01-14 15:57:12 +0100

    Added tests for resolveParentEffectivePom

--- src/test/groovy/it/fulminazzo/conveyor/model/pom/EffectivePomBuilderTest.groovy
+++ src/test/groovy/it/fulminazzo/conveyor/model/pom/EffectivePomBuilderTest.groovy
@@ -1,11 +1,60 @@
 package it.fulminazzo.conveyor.model.pom
 
+import it.fulminazzo.conveyor.model.artifact.Artifact
 import it.fulminazzo.conveyor.model.profile.Profile
 import it.fulminazzo.conveyor.model.profile.activation.Activation
 import it.fulminazzo.conveyor.model.profile.activation.context.ActivationContext
 import spock.lang.Specification
 
 class EffectivePomBuilderTest extends Specification {
+
+    def 'test that resolveParentEffectivePom stores correct parent builder'() {
+        given:
+        def parent = Artifact.builder()
+                .groupId('it.fulminazzo')
+                .artifactId('parent')
+                .version('1.0')
+                .build()
+
+        and:
+        def parentPom = Mock(Pom)
+        parentPom.profiles >> []
+
+        and:
+        PomResolver resolver = p -> parentPom
+
+        and:
+        def pom = Mock(Pom)
+        pom.parent >> parent
+        pom.profiles >> []
+
+        and:
+        def builder = new EffectivePomBuilder(pom, resolver, Mock(ActivationContext))
+
+        when:
+        builder.resolveParentEffectivePom()
+
+        then:
+        builder.parentEffectivePomBuilder != null
+
+        and:
+        builder.parentEffectivePomBuilder.startingPom == parentPom
+    }
+
+    def 'test that resolveParentEffectivePom does not throw on missing parent'() {
+        given:
+        def pom = Mock(Pom)
+        pom.profiles >> []
+
+        and:
+        def builder = new EffectivePomBuilder(pom, (p) -> null, Mock(ActivationContext))
+
+        when:
+        builder.resolveParentEffectivePom()
+
+        then:
+        builder.parentEffectivePomBuilder == null
+    }
 
     def 'test that populateActiveProfiles adds only active profiles'() {
         given:
