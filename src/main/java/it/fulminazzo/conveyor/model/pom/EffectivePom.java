7588773e7d5f1f41be421dcc05d03a7fa82b8632
user <fulminazzo21@gmail.com>
2026-01-14 19:37:41 +0100

    Added constructor to create immutable copy of dependencies

--- src/main/java/it/fulminazzo/conveyor/model/pom/EffectivePom.java
+++ src/main/java/it/fulminazzo/conveyor/model/pom/EffectivePom.java
@@ -3,10 +3,10 @@ package it.fulminazzo.conveyor.model.pom;
 import it.fulminazzo.conveyor.model.Properties;
 import it.fulminazzo.conveyor.model.artifact.Artifact;
 import it.fulminazzo.conveyor.model.dependency.Dependency;
-import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -14,12 +14,29 @@ import java.util.List;
  * with a list of all the required dependencies to make it work.
  */
 @Value
-@RequiredArgsConstructor
 public class EffectivePom implements PomLike {
     @NotNull Artifact project;
     @NotNull String packaging;
 
     @NotNull Properties properties;
     @NotNull List<Dependency> dependencies;
+
+    /**
+     * Instantiates a new Effective pom.
+     *
+     * @param project      the project
+     * @param packaging    the packaging
+     * @param properties   the properties
+     * @param dependencies the dependencies
+     */
+    public EffectivePom(final @NotNull Artifact project,
+                        final @NotNull String packaging,
+                        final @NotNull Properties properties,
+                        final @NotNull Collection<Dependency> dependencies) {
+        this.project = project;
+        this.packaging = packaging;
+        this.properties = properties;
+        this.dependencies = List.copyOf(dependencies);
+    }
 
 }
