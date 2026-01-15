cbe9ae697550ea5d20155e0861ddcd856a0a6f98
user <fulminazzo21@gmail.com>
2026-01-15 11:25:38 +0100

    Implemented resolve method

--- src/main/java/it/fulminazzo/conveyor/downloader/BaseDownloader.java
+++ src/main/java/it/fulminazzo/conveyor/downloader/BaseDownloader.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -17,6 +19,20 @@ final class BaseDownloader implements Downloader {
     private final @NotNull Set<DownloadSource> sources = new HashSet<>();
     @Getter
     private final @NotNull File workingDir;
+
+    @Override
+    public @NotNull InputStream resolve(final @NotNull String resourcePath) throws DownloadException {
+        if (this.sources.isEmpty())
+            throw new DownloadException("No download source provided! Please, use addDownloadSources before calling this method");
+        Throwable latest = null;
+        for (DownloadSource source : this.sources)
+            try {
+                return source.resolveResource(resourcePath);
+            } catch (IOException e) {
+                latest = e;
+            }
+        throw new DownloadException(String.format("Could not download resource '%s'", resourcePath), latest);
+    }
 
     @Override
     public @NotNull Downloader addDownloadSources(final @NotNull Collection<DownloadSource> sources) {
