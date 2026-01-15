1d0c4d44e00388579b42b68d88438f7552c100fb
user <fulminazzo21@gmail.com>
2026-01-15 11:36:04 +0100

    Added tests for resolveToFile

---
+++ src/test/groovy/it/fulminazzo/conveyor/downloader/DownloaderTest.groovy
@@ -0,0 +1,29 @@
+package it.fulminazzo.conveyor.downloader
+
+import it.fulminazzo.conveyor.util.TestUtils
+import spock.lang.Specification
+
+class DownloaderTest extends Specification {
+    private static final File workingDir = new File(TestUtils.BASE_DIR, 'downloader')
+
+    def 'test that resolveToFile correctly saves to path'() {
+        given:
+        if (workingDir.exists()) workingDir.deleteDir()
+
+        and:
+        def downloader = new MockDownloader(workingDir)
+
+        when:
+        def file = downloader.resolveToFile('path/to/resource.txt')
+
+        then:
+        file.exists()
+
+        and:
+        file.absolutePath == workingDir.absolutePath + '/path/to/resource.txt'
+
+        and:
+        file.readLines() == ['Data of \'path/to/resource.txt\'']
+    }
+
+}
