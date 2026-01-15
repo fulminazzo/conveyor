9c7b5c7976dd0d448b96e355c44b95a53990325b
user <fulminazzo21@gmail.com>
2026-01-15 11:40:26 +0100

    Added computeChecksum method

--- src/main/java/it/fulminazzo/conveyor/downloader/ChecksumDownloader.java
+++ src/main/java/it/fulminazzo/conveyor/downloader/ChecksumDownloader.java
@@ -4,8 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collection;
+import java.util.HexFormat;
 
 /**
  * A special type of {@link Downloader} that supports
@@ -17,7 +22,38 @@ import java.util.Collection;
  */
 @RequiredArgsConstructor
 final class ChecksumDownloader implements Downloader {
+    private static final int readingBufferSize = 8192;
+
     private final @NotNull Downloader delegate;
+
+    /**
+     * Computes the checksum for the given file.
+     *
+     * @param resourcePath the resource path
+     * @param algorithm    the algorithm to use
+     * @return the checksum
+     * @throws IOException in case of any errors
+     */
+    @NotNull String computeChecksum(final @NotNull String resourcePath,
+                                    final @NotNull ChecksumAlgorithm algorithm) throws IOException {
+        final File resourceFile = getResourceFile(resourcePath);
+
+        final MessageDigest messageDigest;
+        try {
+            messageDigest = MessageDigest.getInstance(algorithm.name());
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Unreachable code");
+        }
+
+        try (InputStream inputStream = new FileInputStream(resourceFile)) {
+            byte[] buffer = new byte[readingBufferSize];
+            int read;
+            while ((read = inputStream.read(buffer)) != -1)
+                messageDigest.update(buffer, 0, read);
+        }
+
+        return HexFormat.of().formatHex(messageDigest.digest());
+    }
 
     @Override
     public @NotNull InputStream resolve(final @NotNull String resourcePath) throws DownloadException {
