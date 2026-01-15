fee57adba3dd0418555b6958675eaf93fd0049c5
user <fulminazzo21@gmail.com>
2026-01-15 12:48:19 +0100

    Fixed visibility issues Fixed Javadoc

--- src/main/java/it/fulminazzo/conveyor/downloader/DownloadException.java
+++ src/main/java/it/fulminazzo/conveyor/downloader/DownloadException.java
@@ -1,29 +1,11 @@
 package it.fulminazzo.conveyor.downloader;
 
-import org.jetbrains.annotations.NotNull;
+import lombok.experimental.StandardException;
 
 /**
- * An exception thrown by {@link DownloadException}.
+ * An exception thrown by {@link Downloader} objects.
  */
+@StandardException
 public final class DownloadException extends Exception {
-
-    /**
-     * Instantiates a new Download exception.
-     *
-     * @param message the message
-     */
-    DownloadException(final @NotNull String message) {
-        super(message);
-    }
-
-    /**
-     * Instantiates a new Download exception.
-     *
-     * @param message the message
-     * @param cause   the cause
-     */
-    DownloadException(final @NotNull String message, final @NotNull Throwable cause) {
-        super(message, cause);
-    }
 
 }
