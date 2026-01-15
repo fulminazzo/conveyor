ef209b1c98d410d395699c686be84bd6efb8f7e9
user <fulminazzo21@gmail.com>
2026-01-15 22:55:29 +0100

    Updated DependencyNode to not be record Added default exclusions field

--- src/main/java/it/fulminazzo/conveyor/model/tree/DependencyNode.java
+++ src/main/java/it/fulminazzo/conveyor/model/tree/DependencyNode.java
@@ -1,13 +1,20 @@
 package it.fulminazzo.conveyor.model.tree;
 
 import it.fulminazzo.conveyor.model.dependency.Dependency;
+import it.fulminazzo.conveyor.model.dependency.Exclusions;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a node of a {@link Dependency} tree.
- *
- * @param dependency the dependency
- * @param depth the depth of the node
  */
-public record DependencyNode(@NotNull Dependency dependency, int depth) {
+@Data
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class DependencyNode {
+    @NotNull Dependency dependency;
+    int depth;
+    @NotNull Exclusions exclusions = new Exclusions();
+
 }
