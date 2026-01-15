0075240a44f2e86271068034c29014cbf315dcb6
user <fulminazzo21@gmail.com>
2026-01-15 23:21:45 +0100

    Revert "Updated DependencyNode to not be record Added default exclusions field"
    This reverts commit ef209b1c

--- src/main/java/it/fulminazzo/conveyor/model/tree/DependencyNode.java
+++ src/main/java/it/fulminazzo/conveyor/model/tree/DependencyNode.java
@@ -2,21 +2,15 @@ package it.fulminazzo.conveyor.model.tree;
 
 import it.fulminazzo.conveyor.model.dependency.Dependency;
 import it.fulminazzo.conveyor.model.dependency.Exclusions;
-import lombok.*;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a node of a {@link Dependency} tree.
+ *
+ * @param dependency the dependency
+ * @param depth      the depth of the node
  */
-@Value
-@AllArgsConstructor
-public class DependencyNode {
-
-    @NotNull Dependency dependency;
-
-    int depth;
-
-    @NotNull Exclusions exclusions;
+public record DependencyNode(@NotNull Dependency dependency, int depth, @NotNull Exclusions exclusions) {
 
     /**
      * Instantiates a new Dependency node.
