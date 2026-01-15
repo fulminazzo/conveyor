ea749c318ced4f89e69d9e46fac8c474c847da89
user <fulminazzo21@gmail.com>
2026-01-15 19:44:44 +0100

    Added tests for populateTree method

--- src/test/groovy/it/fulminazzo/conveyor/model/tree/DependencyTreeBuilderTest.groovy
+++ src/test/groovy/it/fulminazzo/conveyor/model/tree/DependencyTreeBuilderTest.groovy
@@ -1,6 +1,7 @@
 package it.fulminazzo.conveyor.model.tree
 
 import it.fulminazzo.conveyor.model.artifact.Artifact
+import it.fulminazzo.conveyor.model.dependency.Dependency
 import it.fulminazzo.conveyor.model.dependency.RawDependency
 import it.fulminazzo.conveyor.model.dependency.Scope
 import it.fulminazzo.conveyor.model.pom.Pom
@@ -16,6 +17,77 @@ class DependencyTreeBuilderTest extends Specification {
         this.resolver = Mock(PomResolver)
 
         this.builder = new DependencyTreeBuilder(this.resolver, Mock(ActivationContext))
+    }
+
+    def 'test that populateTree correctly adds new dependencies'() {
+        given:
+        def pom = createFullDependenciesPom('it.fulminazzo', 'main')
+        this.resolver.resolve(_) >> pom
+
+        and:
+        def dependencyNode = new DependencyNode(
+                Dependency.builder()
+                        .groupId('it.fulminazzo')
+                        .artifactId('main')
+                        .version('1.0')
+                        .build(),
+                2
+        )
+
+        and:
+        def dependencies = this.builder.dependenciesToCheck
+
+        when:
+        this.builder.populateTree(dependencyNode)
+
+        then:
+        dependencies.size() == pom.dependencies.size()
+
+        and:
+        for (Scope scope : Scope.values()) {
+            def node = dependencies.find { it.dependency().scope == scope }
+            println "Checking scope: $scope"
+            assert node != null
+            assert node.dependency().artifactId == "dependency-${scope.value()}"
+            assert node.depth() == 3
+        }
+
+        and:
+        this.builder.dependencyTree.get(dependencyNode.dependency().coordinates).depth() == 2
+    }
+
+    def 'test that populateTree does not add new dependencies if dependency already present in dependency tree'() {
+        given:
+        def pom = createFullDependenciesPom('it.fulminazzo', 'main')
+        this.resolver.resolve(_) >> pom
+
+        and:
+        def dependencyNode = new DependencyNode(
+                Dependency.builder()
+                        .groupId('it.fulminazzo')
+                        .artifactId('main')
+                        .version('1.0')
+                        .build(),
+                2
+        )
+
+        and:
+        this.builder.dependencyTree.put(
+                dependencyNode.dependency().coordinates,
+                new DependencyNode(dependencyNode.dependency(), 1)
+        )
+
+        and:
+        def dependencies = this.builder.dependenciesToCheck
+
+        when:
+        this.builder.populateTree(dependencyNode)
+
+        then:
+        dependencies.size() == 0
+
+        and:
+        this.builder.dependencyTree.get(dependencyNode.dependency().coordinates).depth() == 1
     }
 
     def 'test that addPomDependenciesToCheckList adds all dependencies'() {
