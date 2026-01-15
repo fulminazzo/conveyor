685c011176f3c56c3f8040380c216bda93de4cf9
user <fulminazzo21@gmail.com>
2026-01-16 00:52:04 +0100

    Added getFullPath method

--- src/main/java/it/fulminazzo/conveyor/model/artifact/Artifact.java
+++ src/main/java/it/fulminazzo/conveyor/model/artifact/Artifact.java
@@ -17,7 +17,18 @@ public class Artifact extends ArtifactLike {
     private final @NotNull String version;
 
     /**
-     * Gets the path of the artifact in the repository.
+     * Gets the full path of the file representing this artifact
+     * in a repository.
+     *
+     * @param extension the extension
+     * @return the full path
+     */
+    public @NotNull String getFullPath(final @NotNull String extension) {
+        return getPath() + getFileName(extension);
+    }
+
+    /**
+     * Gets the path of the artifact in a repository.
      * <br>
      * The path is defined as:
      * "&lt;groupId&gt;/&lt;artifactId&gt;/&lt;version&gt;/"
