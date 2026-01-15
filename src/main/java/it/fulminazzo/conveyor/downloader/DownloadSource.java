33fdda159f81a95e798586b0b99defbae9e972cd
user <fulminazzo21@gmail.com>
2026-01-15 11:11:13 +0100

    Updated DownloadSource to check if the URL is valid

--- src/main/java/it/fulminazzo/conveyor/downloader/DownloadSource.java
+++ src/main/java/it/fulminazzo/conveyor/downloader/DownloadSource.java
@@ -2,10 +2,12 @@ package it.fulminazzo.conveyor.downloader;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.jetbrains.annotations.NotNull;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -15,13 +17,36 @@ import java.util.Optional;
  */
 @EqualsAndHashCode
 @ToString
-@RequiredArgsConstructor
 public final class DownloadSource {
     @Getter
     private final @NotNull String url;
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
     private final @NotNull Map<Class<?>, Object> capabilities = new HashMap<>();
+
+    /**
+     * Checks if the given URL is valid, then instantiates a new DownloadSource.
+     * <br>
+     * <b>WARNING</b>: only <i>base</i> urls should be passed.
+     * <br>
+     * If the goal is to download a resource in "https://www.example.com/resource/path",
+     * then here only "https://www.example.com/" should be provided.
+     *
+     * @param url the url
+     * @throws MalformedURLException if the URL is invalid
+     */
+    public DownloadSource(final @NotNull String url) throws MalformedURLException {
+        String modifiedUrl = url;
+        String https = "https://";
+        if (!modifiedUrl.startsWith(https) && !modifiedUrl.startsWith("http://")) modifiedUrl = https + modifiedUrl;
+        if (!modifiedUrl.endsWith("/")) modifiedUrl += "/";
+        try {
+            new URI(modifiedUrl);
+        } catch (URISyntaxException e) {
+            throw new MalformedURLException(String.format("Invalid URL '%s'", url));
+        }
+        this.url = modifiedUrl;
+    }
 
     /**
      * Adds the given capability to the current source.
