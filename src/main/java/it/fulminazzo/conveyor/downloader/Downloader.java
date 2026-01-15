9f8839e878dfe8cd1a8848c9762b4a27845fd2fb
user <fulminazzo21@gmail.com>
2026-01-15 11:14:20 +0100

    Created Downloader interface

---
+++ src/main/java/it/fulminazzo/conveyor/downloader/Downloader.java
@@ -0,0 +1,84 @@
+package it.fulminazzo.conveyor.downloader;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * A specialized object to download files to the given path.
+ */
+public interface Downloader {
+
+    /**
+     * Loops through all the download sources trying to download the resource
+     * at "&lt;url&gt;/&lt;resource_path&gt;".
+     * Then, downloads it at "{@link #getWorkingDir()}/&lt;resource_path&gt;".
+     *
+     * @param resourcePath the resource path
+     * @return the newly downloaded file
+     * @throws DownloadException in case of any errors
+     */
+    default @NotNull File resolveToFile(final @NotNull String resourcePath) throws DownloadException {
+        try (InputStream stream = resolve(resourcePath)) {
+            File destination = getResourceFile(resourcePath);
+            Files.createDirectories(destination.getParentFile().toPath());
+            Files.copy(stream, destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            return destination;
+        } catch (IOException e) {
+            throw new DownloadException(String.format("Error while downloading resource '%s'", resourcePath), e);
+        }
+    }
+
+    /**
+     * Loops through all the download sources trying to download the resource
+     * at "&lt;url&gt;/&lt;resource_path&gt;".
+     * Then, returns the resulting stream.
+     *
+     * @param resourcePath the resource path
+     * @return the download stream
+     * @throws DownloadException if the download could not be completed (mostly for resource not found)
+     */
+    @NotNull InputStream resolve(final @NotNull String resourcePath) throws DownloadException;
+
+    /**
+     * Adds all the given {@link DownloadSource}s to the current downloader.
+     *
+     * @param sources the download sources
+     * @return this downloader
+     */
+    default @NotNull Downloader addDownloadSources(final DownloadSource @NotNull ... sources) {
+        return addDownloadSources(Arrays.asList(sources));
+    }
+
+    /**
+     * Adds all the given {@link DownloadSource}s to the current downloader.
+     *
+     * @param sources the download sources
+     * @return this downloader
+     */
+    @NotNull Downloader addDownloadSources(final @NotNull Collection<DownloadSource> sources);
+
+    /**
+     * Gets the associated resource file.
+     *
+     * @param resourcePath the resource path
+     * @return the resource file
+     */
+    default @NotNull File getResourceFile(final @NotNull String resourcePath) {
+        return new File(getWorkingDir(), resourcePath);
+    }
+
+    /**
+     * Gets the working directory (where the downloads will be stored).
+     *
+     * @return the directory
+     */
+    @NotNull File getWorkingDir();
+
+}
