9f0b1c15eb9fe94207bc6cb8e9892ece30ba7096
user <fulminazzo21@gmail.com>
2026-01-16 19:29:47 +0100

    Created Conveyor main class with buildDependencyTree method

---
+++ src/main/java/it/fulminazzo/conveyor/Conveyor.java
@@ -0,0 +1,52 @@
+package it.fulminazzo.conveyor;
+
+import it.fulminazzo.conveyor.model.artifact.Artifact;
+import it.fulminazzo.conveyor.model.pom.resolver.ConveyorPomResolver;
+import it.fulminazzo.conveyor.model.pom.resolver.PomResolverException;
+import it.fulminazzo.conveyor.model.pom.resolver.RepositoryBasedPomResolver;
+import it.fulminazzo.conveyor.model.pom.resolver.engine.PomResolveEngineType;
+import it.fulminazzo.conveyor.model.profile.activation.context.ActivationContext;
+import it.fulminazzo.conveyor.model.tree.DependencyNode;
+import it.fulminazzo.conveyor.model.tree.DependencyTreeBuilder;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+import java.io.File;
+import java.util.Collection;
+
+/**
+ * Main access point of the library.
+ */
+public final class Conveyor {
+    private final @NotNull RepositoryBasedPomResolver resolver;
+    @Deprecated
+    private final @NotNull ActivationContext context;
+
+    /**
+     * Instantiates a new Conveyor.
+     *
+     * @param context    the activation context
+     * @param workingDir the directory where the repositories should be stored
+     * @param logger     the logger
+     */
+    public Conveyor(final @NotNull ActivationContext context,
+                    final @NotNull File workingDir,
+                    final @NotNull Logger logger) {
+        this.resolver = ConveyorPomResolver.newResolver(workingDir, logger).setMode(PomResolveEngineType.CHECKSUM);
+        this.context = context;
+    }
+
+    /**
+     * Builds the dependency tree of the given artifact.
+     *
+     * @param artifact the artifact
+     * @return the dependency tree
+     * @throws PomResolverException in case of any errors
+     */
+    @NotNull Collection<DependencyNode> buildDependencyTree(final @NotNull Artifact artifact) throws PomResolverException {
+        //TODO: activation context requires packaging,
+        //TODO: but packaging is resolved in pom!
+        return new DependencyTreeBuilder(artifact, this.resolver, this.context).build();
+    }
+
+}
