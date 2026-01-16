12e892d1783265b3d8819aabcc0c61db98ebc4ae
user <fulminazzo21@gmail.com>
2026-01-16 18:54:15 +0100

    Created MockRepositoryBasedPomResolver for testing purposes

---
+++ src/test/java/it/fulminazzo/conveyor/model/pom/resolver/MockRepositoryBasedPomResolver.java
@@ -0,0 +1,31 @@
+package it.fulminazzo.conveyor.model.pom.resolver;
+
+import it.fulminazzo.conveyor.downloader.DownloadSource;
+import it.fulminazzo.conveyor.model.repository.Repository;
+import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+final class MockRepositoryBasedPomResolver extends BaseRepositoryBasedPomResolver {
+
+    public MockRepositoryBasedPomResolver(final @NotNull File workingDir,
+                                          final @NotNull Logger logger) {
+        super(workingDir, logger);
+    }
+
+    @SneakyThrows
+    @Override
+    public @NotNull RepositoryBasedPomResolver addRepositories(final @NotNull Collection<Repository> repositories) {
+        getEngine().addSources(repositories.stream()
+                .map(Repository::getUrl)
+                .map(DownloadSource::new)
+                .collect(Collectors.toSet())
+        );
+        return this;
+    }
+
+}
