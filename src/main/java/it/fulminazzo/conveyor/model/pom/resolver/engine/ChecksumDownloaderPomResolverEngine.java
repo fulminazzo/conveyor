b12239d6effb0ff8fa26683eca1ce124eb8cde20
user <fulminazzo21@gmail.com>
2026-01-16 14:01:49 +0100

    Created ChecksumDownloaderPomResolverEngine

---
+++ src/main/java/it/fulminazzo/conveyor/model/pom/resolver/engine/ChecksumDownloaderPomResolverEngine.java
@@ -0,0 +1,43 @@
+package it.fulminazzo.conveyor.model.pom.resolver.engine;
+
+import it.fulminazzo.conveyor.downloader.DownloadException;
+import it.fulminazzo.conveyor.downloader.Downloader;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * An implementation of {@link DownloaderPomResolverEngine} that
+ * checks if the pom is already present, if it is it attempts
+ * to compute and verify its checksum, otherwise proceeds to
+ * download the resource.
+ */
+final class ChecksumDownloaderPomResolverEngine extends DownloaderPomResolverEngine<Downloader> {
+
+    /**
+     * Instantiates a new Checksum downloader pom resolver engine.
+     *
+     * @param workingDir the working dir
+     * @param logger     the logger
+     */
+    public ChecksumDownloaderPomResolverEngine(final @NotNull File workingDir,
+                                               final @NotNull Logger logger) {
+        super(Downloader.newChecksumDownloader(workingDir, logger));
+    }
+
+    @Override
+    protected @NotNull InputStream resolve(final @NotNull String artifactPath) throws DownloadException {
+        try {
+            File file = this.downloader.resolveToFile(artifactPath);
+            return new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            // should be impossible
+            throw new DownloadException(e);
+        }
+    }
+
+}
