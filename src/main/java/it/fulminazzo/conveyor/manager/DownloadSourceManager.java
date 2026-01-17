a20f0ea8df7f7f9543db848445ea1accbedaafd1
user <fulminazzo21@gmail.com>
2026-01-17 11:28:34 +0100

    Created DownloadSourceManager to keep track of DownloadSources

---
+++ src/main/java/it/fulminazzo/conveyor/manager/DownloadSourceManager.java
@@ -0,0 +1,39 @@
+package it.fulminazzo.conveyor.manager;
+
+import it.fulminazzo.conveyor.downloader.DownloadSource;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Keeps track of all the {@link DownloadSource}s.
+ */
+public interface DownloadSourceManager {
+
+    /**
+     * Gets the currently stored download sources.
+     *
+     * @return the download sources
+     */
+    @NotNull Collection<DownloadSource> getDownloadSources();
+
+    /**
+     * Adds all the given {@link DownloadSource}s to the list.
+     *
+     * @param sources the download sources
+     * @return this manager
+     */
+    default @NotNull DownloadSourceManager addDownloadSources(final DownloadSource @NotNull ... sources) {
+        return addDownloadSources(Arrays.asList(sources));
+    }
+
+    /**
+     * Adds all the given {@link DownloadSource}s to the list.
+     *
+     * @param sources the download sources
+     * @return this manager
+     */
+    @NotNull DownloadSourceManager addDownloadSources(final @NotNull Collection<DownloadSource> sources);
+
+}
