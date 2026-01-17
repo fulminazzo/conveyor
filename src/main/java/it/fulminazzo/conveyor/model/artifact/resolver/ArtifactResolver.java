6eac583472a487e4f4aa3d0098460575d8e5ca06
user <fulminazzo21@gmail.com>
2026-01-17 13:49:02 +0100

    Added Javadoc and updated resolve method

--- src/main/java/it/fulminazzo/conveyor/model/artifact/resolver/ArtifactResolver.java
+++ src/main/java/it/fulminazzo/conveyor/model/artifact/resolver/ArtifactResolver.java
@@ -5,8 +5,19 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
+/**
+ * An object to resolve an artifact package given its information and packaging.
+ */
 public interface ArtifactResolver {
 
-    @NotNull File resolve(final @NotNull Artifact artifact);
+    /**
+     * Resolves the artifact package and returns the corresponding file.
+     *
+     * @param artifact  the artifact
+     * @param packaging the packaging
+     * @return the artifact file
+     * @throws ArtifactResolverException in case of any exceptions
+     */
+    @NotNull File resolve(final @NotNull Artifact artifact, final @NotNull String packaging) throws ArtifactResolverException;
 
 }
