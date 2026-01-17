307b6a21988e399bcabfa36a66f6f574a393986d
user <fulminazzo21@gmail.com>
2026-01-17 12:45:45 +0100

    Copied RepositoryBasedPomResolver from resolver package

---
+++ src/main/java/it/fulminazzo/conveyor/model/pom/new_resolver/RepositoryBasedPomResolver.java
@@ -0,0 +1,26 @@
+package it.fulminazzo.conveyor.model.pom.new_resolver;
+
+import it.fulminazzo.conveyor.model.artifact.Artifact;
+import it.fulminazzo.conveyor.model.pom.Pom;
+import it.fulminazzo.conveyor.model.repository.Repository;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+/**
+ * An object to resolve a {@link Pom} object given its {@link Artifact} information.
+ * <br>
+ * Uses repositories to query for the requested data.
+ */
+public interface RepositoryBasedPomResolver extends PomResolver {
+
+    /**
+     * Adds repositories to the repository manager of the resolver.
+     *
+     * @param repositories the repositories
+     * @return this resolver
+     */
+    @NotNull RepositoryBasedPomResolver addRepositories(final @NotNull Collection<Repository> repositories);
+
+
+}
