496787c3e60ea646992937fda5b3b87537701899
user <fulminazzo21@gmail.com>
2026-01-17 11:59:30 +0100

    Implemented addRepositories method

--- src/main/java/it/fulminazzo/conveyor/manager/RepositoryManagerImpl.java
+++ src/main/java/it/fulminazzo/conveyor/manager/RepositoryManagerImpl.java
@@ -1,9 +1,11 @@
 package it.fulminazzo.conveyor.manager;
 
 import it.fulminazzo.conveyor.downloader.DownloadSource;
+import it.fulminazzo.conveyor.downloader.policy.ChecksumPolicies;
 import it.fulminazzo.conveyor.model.repository.Repository;
 import org.jetbrains.annotations.NotNull;
 
+import java.net.MalformedURLException;
 import java.util.Collection;
 
 /**
@@ -15,7 +17,21 @@ final class RepositoryManagerImpl implements RepositoryManager {
 
     @Override
     public @NotNull RepositoryManager addRepositories(final @NotNull Collection<Repository> repositories) {
-        throw new UnsupportedOperationException();
+        for (Repository repository : repositories) {
+            try {
+                Repository.Policy releases = repository.getReleases();
+                if (releases.isEnabled())
+                    this.releases.addDownloadSources(toDownloadSource(repository, releases));
+
+                Repository.Policy snapshots = repository.getSnapshots();
+                if (snapshots.isEnabled())
+                    this.snapshots.addDownloadSources(toDownloadSource(repository, snapshots));
+            } catch (MalformedURLException e) {
+                //TODO: logging?
+                throw new RuntimeException(e);
+            }
+        }
+        return this;
     }
 
     @Override
@@ -26,6 +42,12 @@ final class RepositoryManagerImpl implements RepositoryManager {
     @Override
     public @NotNull Collection<DownloadSource> getSnapshotsRepositories() {
         return this.snapshots.getDownloadSources();
+    }
+
+    private static @NotNull DownloadSource toDownloadSource(final @NotNull Repository repository,
+                                                            final @NotNull Repository.Policy policy) throws MalformedURLException {
+        return new DownloadSource(repository.getUrl())
+                .withCapability(ChecksumPolicies.valueOf(policy.getChecksumPolicy().name()));
     }
 
 }
