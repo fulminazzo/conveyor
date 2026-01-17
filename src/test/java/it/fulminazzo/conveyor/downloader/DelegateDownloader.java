d3d9d2934c176642e116c6707f8777dc93b8071e
user <fulminazzo21@gmail.com>
2026-01-17 14:35:52 +0100

    Optimized DelegateDownloader with Delegate annotation

--- src/test/java/it/fulminazzo/conveyor/downloader/DelegateDownloader.java
+++ src/test/java/it/fulminazzo/conveyor/downloader/DelegateDownloader.java
@@ -1,25 +1,12 @@
 package it.fulminazzo.conveyor.downloader;
 
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.Delegate;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
-import java.io.InputStream;
-import java.util.Collection;
 
 @RequiredArgsConstructor
 public final class DelegateDownloader implements Downloader {
+    @Delegate
     private final @NotNull Downloader delegate;
-
-    @Override
-    public @NotNull InputStream resolve(@NotNull String resourcePath,
-                                        final @NotNull Collection<DownloadSource> downloadSources) throws DownloadException {
-        return this.delegate.resolve(resourcePath, downloadSources);
-    }
-
-    @Override
-    public @NotNull File getWorkingDir() {
-        return this.delegate.getWorkingDir();
-    }
 
 }
