20494ce349af0236f4d50c62019ebced644009b8
user <fulminazzo21@gmail.com>
2026-01-17 16:05:18 +0100

    Updated ArtifactResolverMode to utilize newly created Disk and Checksum resolvers

--- src/main/java/it/fulminazzo/conveyor/model/artifact/resolver/mode/ArtifactResolverMode.java
+++ src/main/java/it/fulminazzo/conveyor/model/artifact/resolver/mode/ArtifactResolverMode.java
@@ -1,19 +1,15 @@
 package it.fulminazzo.conveyor.model.artifact.resolver.mode;
 
-import it.fulminazzo.conveyor.downloader.Downloader;
 import it.fulminazzo.conveyor.manager.RepositoryManager;
 import it.fulminazzo.conveyor.model.artifact.resolver.ArtifactResolver;
-import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
 import java.io.File;
-import java.util.function.BiFunction;
 
 /**
  * Defines the operating mode of the resolver.
  */
-@RequiredArgsConstructor
 public enum ArtifactResolverMode {
     /**
      * It will lookup the file on disk.
@@ -21,7 +17,14 @@ public enum ArtifactResolverMode {
      * Otherwise, the download procedure will begin.
      * Subsequent requests will use the newly downloaded file.
      */
-    DISK(Downloader::newDownloader),
+    DISK {
+        @Override
+        public @NotNull ArtifactResolver create(final @NotNull RepositoryManager repositoryManager,
+                                                final @NotNull File workingDir,
+                                                final @NotNull Logger logger) {
+            return new DiskDownloaderArtifactResolver(repositoryManager, workingDir, logger);
+        }
+    },
     /**
      * It will lookup the file on disk.
      * If present, it will be verified with a checksum.
@@ -29,9 +32,14 @@ public enum ArtifactResolverMode {
      * the download source might specify the next course of action
      * (failure, ignoring or re-downloading the resource).
      */
-    CHECKSUM(Downloader::newChecksumDownloader);
-
-    private final @NotNull BiFunction<File, Logger, Downloader> downloaderSupplier;
+    CHECKSUM {
+        @Override
+        public @NotNull ArtifactResolver create(final @NotNull RepositoryManager repositoryManager,
+                                                final @NotNull File workingDir,
+                                                final @NotNull Logger logger) {
+            return new ChecksumDownloaderArtifactResolver(repositoryManager, workingDir, logger);
+        }
+    };
 
     /**
      * Creates a new Artifact resolver.
@@ -41,13 +49,8 @@ public enum ArtifactResolverMode {
      * @param logger            the logger
      * @return the artifact resolver
      */
-    public @NotNull ArtifactResolver create(final @NotNull RepositoryManager repositoryManager,
-                                            final @NotNull File workingDir,
-                                            final @NotNull Logger logger) {
-        return new DownloaderArtifactResolver(
-                repositoryManager,
-                this.downloaderSupplier.apply(workingDir, logger)
-        );
-    }
+    public abstract @NotNull ArtifactResolver create(final @NotNull RepositoryManager repositoryManager,
+                                                     final @NotNull File workingDir,
+                                                     final @NotNull Logger logger);
 
 }
