f147b37615b6381ca7f96ba71a2410fee273b61f
user <fulminazzo21@gmail.com>
2026-01-17 11:48:45 +0100

    Created RepositoryManager interface

---
+++ src/main/java/it/fulminazzo/conveyor/manager/RepositoryManager.java
@@ -0,0 +1,50 @@
+package it.fulminazzo.conveyor.manager;
+
+import it.fulminazzo.conveyor.downloader.DownloadSource;
+import it.fulminazzo.conveyor.model.repository.Repository;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Handles all the given {@link Repository},
+ * separating them based on the {@link Repository.Policy} provided.
+ */
+public interface RepositoryManager {
+
+    /**
+     * Adds the repositories to the list.
+     *
+     * @param repositories the repositories
+     * @return this manager
+     */
+    default @NotNull RepositoryManager addRepositories(final Repository @NotNull ... repositories) {
+        return addRepositories(Arrays.asList(repositories));
+    }
+
+    /**
+     * Adds the repositories to the list.
+     *
+     * @param repositories the repositories
+     * @return this manager
+     */
+    @NotNull RepositoryManager addRepositories(final @NotNull Collection<Repository> repositories);
+
+    /**
+     * Gets all the repositories with the <b>releases</b>
+     * {@link Repository.Policy#isEnabled()} set to true.
+     *
+     * @return the repositories
+     */
+    @NotNull Collection<DownloadSource> getReleasesRepositories();
+
+    /**
+     * Gets all the repositories with the <b>snapshots</b>
+     * {@link Repository.Policy#isEnabled()} set to true.
+     *
+     * @return the repositories
+     */
+    @NotNull Collection<DownloadSource> getSnapshotsRepositories();
+
+}
