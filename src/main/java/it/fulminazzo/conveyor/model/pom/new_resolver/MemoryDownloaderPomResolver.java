4482928c6626c604a0f095d28735c21aaacde6fa
user <fulminazzo21@gmail.com>
2026-01-17 12:33:42 +0100

    Updated MemoryDownloaderPomResolver with internal downloader field and resolve method with sources

--- src/main/java/it/fulminazzo/conveyor/model/pom/new_resolver/MemoryDownloaderPomResolver.java
+++ src/main/java/it/fulminazzo/conveyor/model/pom/new_resolver/MemoryDownloaderPomResolver.java
@@ -1,6 +1,7 @@
 package it.fulminazzo.conveyor.model.pom.new_resolver;
 
 import it.fulminazzo.conveyor.downloader.DownloadException;
+import it.fulminazzo.conveyor.downloader.DownloadSource;
 import it.fulminazzo.conveyor.downloader.Downloader;
 import it.fulminazzo.conveyor.manager.RepositoryManager;
 import org.jetbrains.annotations.NotNull;
@@ -8,12 +9,14 @@ import org.slf4j.Logger;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.Collection;
 
 /**
- * An implementation of {@link DownloaderPomResolver} that
+ * An implementation of {@link BasePomResolver} that
  * downloads the requested pom in every request.
  */
-final class MemoryDownloaderPomResolver extends DownloaderPomResolver {
+final class MemoryDownloaderPomResolver extends BasePomResolver {
+    private final @NotNull Downloader downloader;
 
     /**
      * Instantiates a new Memory downloader pom resolver.
@@ -25,12 +28,14 @@ final class MemoryDownloaderPomResolver extends DownloaderPomResolver {
     public MemoryDownloaderPomResolver(final @NotNull RepositoryManager repositoryManager,
                                        final @NotNull File workingDir,
                                        final @NotNull Logger logger) {
-        super(repositoryManager, Downloader.newDownloader(workingDir, logger));
+        super(repositoryManager);
+        this.downloader = Downloader.newDownloader(workingDir, logger);
     }
 
     @Override
-    protected @NotNull InputStream resolve(@NotNull String artifactPath) throws DownloadException {
-        return this.downloader.resolve(artifactPath);
+    protected @NotNull InputStream resolve(final @NotNull String artifactPath,
+                                           final @NotNull Collection<DownloadSource> downloadSources) throws DownloadException {
+        return this.downloader.resolve(artifactPath, downloadSources);
     }
 
 }
