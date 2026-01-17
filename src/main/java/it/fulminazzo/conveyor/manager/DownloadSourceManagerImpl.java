0d72278bb0c9c99585d4d53a7d28bed3fdaee75b
user <fulminazzo21@gmail.com>
2026-01-17 11:50:16 +0100

    Created DownloadSourceManagerImpl

---
+++ src/main/java/it/fulminazzo/conveyor/manager/DownloadSourceManagerImpl.java
@@ -0,0 +1,27 @@
+package it.fulminazzo.conveyor.manager;
+
+import it.fulminazzo.conveyor.downloader.DownloadSource;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A basic implementation for {@link DownloadSourceManager}.
+ */
+final class DownloadSourceManagerImpl implements DownloadSourceManager {
+    private final @NotNull Set<DownloadSource> downloadSources = new HashSet<>();
+
+    @Override
+    public @NotNull DownloadSourceManager addDownloadSources(final @NotNull Collection<DownloadSource> sources) {
+        this.downloadSources.addAll(sources);
+        return this;
+    }
+
+    @Override
+    public @NotNull Collection<DownloadSource> getDownloadSources() {
+        return this.downloadSources;
+    }
+
+}
