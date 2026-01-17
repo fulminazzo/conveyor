b8cd96efa5131c0a0f6ac7934c303da3b0405411
user <fulminazzo21@gmail.com>
2026-01-17 11:36:06 +0100

    Updated MockDownloader resolve method Removed downloadSources field and related methods

--- src/test/java/it/fulminazzo/conveyor/downloader/MockDownloader.java
+++ src/test/java/it/fulminazzo/conveyor/downloader/MockDownloader.java
@@ -8,24 +8,16 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 @Getter
 @RequiredArgsConstructor
 final class MockDownloader implements Downloader {
-    private final @NotNull Set<DownloadSource> downloadSources = new HashSet<>();
     private final @NotNull File workingDir;
 
     @Override
-    public @NotNull InputStream resolve(final @NotNull String resourcePath) {
+    public @NotNull InputStream resolve(final @NotNull String resourcePath,
+                                        final @NotNull Collection<DownloadSource> downloadSources) {
         return new ByteArrayInputStream(String.format("Data of '%s'", resourcePath).getBytes());
-    }
-
-    @Override
-    public @NotNull Downloader addDownloadSources(final @NotNull Collection<DownloadSource> sources) {
-        this.downloadSources.addAll(sources);
-        return this;
     }
 
 }
