63a6f27bce7ffc285d2ef308935287edd4c4ce3f
user <fulminazzo21@gmail.com>
2026-01-17 14:28:06 +0100

    Refactored code

--- src/main/java/it/fulminazzo/conveyor/downloader/Downloader.java
+++ src/main/java/it/fulminazzo/conveyor/downloader/Downloader.java
@@ -4,10 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.util.Collection;
 
 /**
