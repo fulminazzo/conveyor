716de12680d028054c56a082c5626726ab1620e4
user <fulminazzo21@gmail.com>
2026-01-18 17:57:05 +0100

    Refactored code

--- src/main/java/it/fulminazzo/conveyor/model/properties/EnvProperties.java
+++ src/main/java/it/fulminazzo/conveyor/model/properties/EnvProperties.java
@@ -8,10 +8,10 @@ import org.jetbrains.annotations.Nullable;
  * all the currently stored environment properties.
  */
 final class EnvProperties extends BaseProperties {
+    private static final @NotNull String prefix = "env.";
 
     @Override
     public @Nullable String get(final @NotNull String key) {
-        final String prefix = "env.";
         if (key.startsWith(prefix))
             return System.getenv(key.substring(prefix.length()));
         else return null;
