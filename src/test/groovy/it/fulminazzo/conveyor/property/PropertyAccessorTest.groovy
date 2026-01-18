888781771da14fe4f1998a1f6e9ef12781e371a0
user <fulminazzo21@gmail.com>
2026-01-18 19:03:03 +0100

    Added tests for invokeMethod

--- src/test/groovy/it/fulminazzo/conveyor/property/PropertyAccessorTest.groovy
+++ src/test/groovy/it/fulminazzo/conveyor/property/PropertyAccessorTest.groovy
@@ -23,4 +23,24 @@ class PropertyAccessorTest extends Specification {
         'field5' || null
     }
 
+    def 'test that invokeMethod of #name returns #expected'() {
+        given:
+        def object = new MockObject()
+
+        when:
+        def value = PropertyAccessor.invokeMethod(object, name)
+
+        then:
+        value == expected
+
+        where:
+        name      || expected
+        'method1' || 'Hello, world!'
+        'method2' || '10'
+        'method3' || 'true'
+        'method4' || null
+        'method5' || null
+        'method6' || null
+    }
+
 }
