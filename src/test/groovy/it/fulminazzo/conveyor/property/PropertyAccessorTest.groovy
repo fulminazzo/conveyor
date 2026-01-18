a3634dda96f0211ce225450106a9d288edfe0227
user <fulminazzo21@gmail.com>
2026-01-18 18:58:33 +0100

    Added tests for getField function

---
+++ src/test/groovy/it/fulminazzo/conveyor/property/PropertyAccessorTest.groovy
@@ -0,0 +1,26 @@
+package it.fulminazzo.conveyor.property
+
+import spock.lang.Specification
+
+class PropertyAccessorTest extends Specification {
+
+    def 'test that getField of #name returns #expected'() {
+        given:
+        def object = new MockObject()
+
+        when:
+        def value = PropertyAccessor.getField(object, name)
+
+        then:
+        value == expected
+
+        where:
+        name     || expected
+        'field1' || 'Hello, world!'
+        'field2' || '10'
+        'field3' || 'true'
+        'field4' || null
+        'field5' || null
+    }
+
+}
