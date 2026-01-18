1f8c7fdd22415f01ea909beba2132d864e2be0ee
user <fulminazzo21@gmail.com>
2026-01-19 00:09:19 +0100

    Created Plugin class with Execution subclass

---
+++ src/main/java/it/fulminazzo/conveyor/model/pom/metadata/Plugin.java
@@ -0,0 +1,51 @@
+package it.fulminazzo.conveyor.model.pom.metadata;
+
+import it.fulminazzo.conveyor.model.dependency.RawDependency;
+import lombok.Builder;
+import lombok.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Value
+@Builder
+public class Plugin {
+
+    @Builder.Default
+    @NotNull String groupId = "org.apache.maven.plugins";
+
+    @Nullable String artifactId;
+
+    @Nullable String version;
+
+    @Nullable String extensions = "false";
+
+    @Builder.Default
+    @NotNull List<Execution> executions = new LinkedList<>();
+
+    @Builder.Default
+    @NotNull List<RawDependency> dependencies = new LinkedList<>();
+
+    @Builder.Default
+    @Nullable String inherited = "true";
+
+    @Value
+    @Builder
+    static class Execution {
+
+        @Builder.Default
+        @NotNull String id = "default";
+
+        @Nullable String phase;
+
+        @Builder.Default
+        @NotNull List<String> goals = new LinkedList<>();
+
+        @Builder.Default
+        @Nullable String inherited = "true";
+
+    }
+
+}
