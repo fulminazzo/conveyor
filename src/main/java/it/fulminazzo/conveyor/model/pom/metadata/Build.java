8fffd45db6bcbf046bce81babfda7292322ae4fc
user <fulminazzo21@gmail.com>
2026-01-19 00:09:54 +0100

    Created Build

---
+++ src/main/java/it/fulminazzo/conveyor/model/pom/metadata/Build.java
@@ -0,0 +1,59 @@
+package it.fulminazzo.conveyor.model.pom.metadata;
+
+import it.fulminazzo.conveyor.model.artifact.Artifact;
+import lombok.Builder;
+import lombok.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+@Value
+@Builder
+public class Build {
+
+    @Builder.Default
+    @NotNull String sourceDirectory = "src/main/java";
+
+    @Builder.Default
+    @NotNull String scriptSourceDirectory = "src/main/scripts";
+
+    @Builder.Default
+    @NotNull String testSourceDirectory = "src/test/java";
+
+    @Builder.Default
+    @NotNull String outputDirectory = "target/classes";
+
+    @Builder.Default
+    @NotNull String testOutputDirectory = "target/test-classes";
+
+    @Builder.Default
+    @NotNull List<Artifact> extensions = new LinkedList<>();
+
+    @Nullable String defaultGoal;
+
+    @Builder.Default
+    @NotNull List<Resource> resource = new LinkedList<>();
+
+    @Builder.Default
+    @NotNull List<Resource> testResource = new LinkedList<>();
+
+    @Builder.Default
+    @NotNull String directory = "target";
+
+    @Builder.Default
+    @NotNull String finalName = "${artifactId}-${version}";
+
+    @Builder.Default
+    @NotNull List<String> filters = new LinkedList<>();
+
+    @Builder.Default
+    @NotNull Set<Plugin> pluginManagement = new HashSet<>();
+
+    @Builder.Default
+    @NotNull List<Plugin> plugins = new LinkedList<>();
+
+}
