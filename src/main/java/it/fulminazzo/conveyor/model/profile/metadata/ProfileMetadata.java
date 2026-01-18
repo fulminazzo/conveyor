881125f5011d4cfe8bc5d2af9e3286bda4143943
user <fulminazzo21@gmail.com>
2026-01-19 00:59:02 +0100

    Added build field

--- src/main/java/it/fulminazzo/conveyor/model/profile/metadata/ProfileMetadata.java
+++ src/main/java/it/fulminazzo/conveyor/model/profile/metadata/ProfileMetadata.java
@@ -1,12 +1,10 @@
 package it.fulminazzo.conveyor.model.profile.metadata;
 
 import it.fulminazzo.conveyor.model.metadata.MavenModelMetadata;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents the {@link it.fulminazzo.conveyor.model.profile.Profile} metadata.
@@ -17,5 +15,8 @@ import lombok.experimental.SuperBuilder;
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @SuperBuilder
 public final class ProfileMetadata extends MavenModelMetadata {
+
+    @Builder.Default
+    @NotNull BuildBase build = BuildBase.builder().build();
 
 }
