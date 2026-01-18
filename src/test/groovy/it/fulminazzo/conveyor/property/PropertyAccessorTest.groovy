f3545461e3ade69312edfd5d33f588652ef9b654
user <fulminazzo21@gmail.com>
2026-01-18 19:19:19 +0100

    Added tests for getIndexed functions

--- src/test/groovy/it/fulminazzo/conveyor/property/PropertyAccessorTest.groovy
+++ src/test/groovy/it/fulminazzo/conveyor/property/PropertyAccessorTest.groovy
@@ -4,6 +4,39 @@ import spock.lang.Specification
 
 class PropertyAccessorTest extends Specification {
 
+    def 'test that getIndexed of #name with #index returns #expected'() {
+        given:
+        def object = new MockObject()
+
+        when:
+        def value = PropertyAccessor.getIndexed(object, name, index)
+
+        then:
+        value == expected
+
+        where:
+        name       | index || expected
+        'indexed1' | 0     || 'Hello'
+        'indexed1' | '0'   || 'Hello'
+        'indexed2' | 2     || 3
+        'indexed2' | '2'   || 3
+        'indexed3' | 1     || false
+        'indexed3' | '1'   || false
+        'indexed4' | 1     || 'world'
+        'indexed4' | '1'   || 'world'
+    }
+
+    def 'test that getIndexed of non indexable object throws'() {
+        given:
+        def object = new MockObject()
+
+        when:
+        PropertyAccessor.getIndexed(object, 'field1', 0)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
     def 'test that getField of #name returns #expected'() {
         given:
         def object = new MockObject()
