41f1f0f32e5c40b76b7e2ef3af66cb5b13ed5478
user <fulminazzo21@gmail.com>
2026-01-18 20:34:09 +0100

    Updated tests

--- src/test/groovy/it/fulminazzo/conveyor/model/pom/PomTest.groovy
+++ src/test/groovy/it/fulminazzo/conveyor/model/pom/PomTest.groovy
@@ -8,12 +8,14 @@ class PomTest extends Specification {
     def 'test that getProperty of #key returns #expected'() {
         given:
         def pom = Pom.builder()
+                .modelVersion('4.0.0')
                 .parent(new Artifact('it.fulminazzo', 'conveyor-parent', '1.0'))
                 .project(new Artifact('it.fulminazzo', 'conveyor', '1.0'))
                 .packaging('war')
                 .name('conveyor')
                 .description('A maven library...')
                 .url('fulminazzo.it')
+                .inceptionYear('2024')
                 .build()
 
         when:
@@ -24,7 +26,7 @@ class PomTest extends Specification {
 
         where:
         key                 || expected
-        'modelVersion'      || null //TODO:
+        'modelVersion'      || '4.0.0'
         'parent.groupId'    || 'it.fulminazzo'
         'parent.artifactId' || 'conveyor-parent'
         'parent.version'    || '1.0'
@@ -36,7 +38,7 @@ class PomTest extends Specification {
         'name'              || 'conveyor'
         'description'       || 'A maven library...'
         'url'               || 'fulminazzo.it'
-        'inceptionYear'     || null //TODO:
+        'inceptionYear'     || '2024'
     }
 
 }
