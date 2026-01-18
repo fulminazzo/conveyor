85809c39be09d3291fd38c410db8264e586b2c30
user <fulminazzo21@gmail.com>
2026-01-18 17:54:32 +0100

    Added tests for SystemProperties

---
+++ src/test/groovy/it/fulminazzo/conveyor/model/properties/SystemPropertiesTest.groovy
@@ -0,0 +1,21 @@
+package it.fulminazzo.conveyor.model.properties
+
+import spock.lang.Specification
+
+class SystemPropertiesTest extends Specification {
+
+    def 'test that SystemProperties returns system property'() {
+        given:
+        def properties = new SystemProperties()
+
+        when:
+        def property = properties.get('os.name')
+
+        then:
+        property != null
+
+        and:
+        property == System.getProperty('os.name')
+    }
+
+}
