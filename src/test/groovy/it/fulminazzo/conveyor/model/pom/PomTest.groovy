d58d0b330db6ef8ee0995541ae4d062b7d37a16e
user <fulminazzo21@gmail.com>
2026-01-18 22:25:40 +0100

    Added more tests for developers related properties

--- src/test/groovy/it/fulminazzo/conveyor/model/pom/PomTest.groovy
+++ src/test/groovy/it/fulminazzo/conveyor/model/pom/PomTest.groovy
@@ -1,6 +1,7 @@
 package it.fulminazzo.conveyor.model.pom
 
 import it.fulminazzo.conveyor.model.artifact.Artifact
+import it.fulminazzo.conveyor.model.pom.metadata.Developer
 import it.fulminazzo.conveyor.model.pom.metadata.License
 import it.fulminazzo.conveyor.model.pom.metadata.Organization
 import it.fulminazzo.conveyor.model.pom.metadata.PomMetadata
@@ -38,6 +39,30 @@ class PomTest extends Specification {
                                         .comments('comments2')
                                         .build()
                         ].toSet())
+                        .developers(new HashSet<>([
+                                Developer.builder()
+                                        .id('fulminazzo')
+                                        .name('Alex')
+                                        .email('alex@fulminazzo.it')
+                                        .url('alex.fulminazzo.it')
+                                        .organization('Fulminazzo')
+                                        .organizationUrl('fulminazzo.it')
+                                        .roles(['founder'])
+                                        .timezone('+1')
+                                        .properties(['contribution': '0%'])
+                                        .build(),
+                                Developer.builder()
+                                        .id('camu')
+                                        .name('Camilla')
+                                        .email('camu@fulminazzo.it')
+                                        .url('camu.fulminazzo.it')
+                                        .organization('Fulminazzo')
+                                        .organizationUrl('fulminazzo.it')
+                                        .roles(['co-founder', 'accountant'])
+                                        .timezone('+1')
+                                        .properties(['contribution': '100%'])
+                                        .build()
+                        ]))
                         .build())
                 .build()
 
@@ -48,30 +73,49 @@ class PomTest extends Specification {
         value == expected
 
         where:
-        key                        || expected
-        'modelVersion'             || '4.0.0'
-        'parent.groupId'           || 'it.fulminazzo'
-        'parent.artifactId'        || 'conveyor-parent'
-        'parent.version'           || '1.0'
+        key                             || expected
+        'modelVersion'                  || '4.0.0'
+        'parent.groupId'                || 'it.fulminazzo'
+        'parent.artifactId'             || 'conveyor-parent'
+        'parent.version'                || '1.0'
         // 'parent.relativePath' || NOT SUPPORTED
-        'groupId'                  || 'it.fulminazzo'
-        'artifactId'               || 'conveyor'
-        'version'                  || '1.0'
-        'packaging'                || 'war'
-        'name'                     || 'conveyor'
-        'description'              || 'A maven library...'
-        'url'                      || 'fulminazzo.it'
-        'inceptionYear'            || '2024'
-        'organization.name'        || 'fulminazzo'
-        'organization.url'         || 'fulminazzo.it'
-        'licenses[0].name'         || 'license1'
-        'licenses[0].url'          || 'license1.com'
-        'licenses[0].distribution' || 'distribution1'
-        'licenses[0].comments'     || 'comments1'
-        'licenses[1].name'         || 'license2'
-        'licenses[1].url'          || 'license2.com'
-        'licenses[1].distribution' || 'distribution2'
-        'licenses[1].comments'     || 'comments2'
+        'groupId'                       || 'it.fulminazzo'
+        'artifactId'                    || 'conveyor'
+        'version'                       || '1.0'
+        'packaging'                     || 'war'
+        'name'                          || 'conveyor'
+        'description'                   || 'A maven library...'
+        'url'                           || 'fulminazzo.it'
+        'inceptionYear'                 || '2024'
+        'organization.name'             || 'fulminazzo'
+        'organization.url'              || 'fulminazzo.it'
+        'licenses[0].name'              || 'license1'
+        'licenses[0].url'               || 'license1.com'
+        'licenses[0].distribution'      || 'distribution1'
+        'licenses[0].comments'          || 'comments1'
+        'licenses[1].name'              || 'license2'
+        'licenses[1].url'               || 'license2.com'
+        'licenses[1].distribution'      || 'distribution2'
+        'licenses[1].comments'          || 'comments2'
+        'developers[0].id'              || 'fulminazzo'
+        'developers[0].name'            || 'Alex'
+        'developers[0].email'           || 'alex@fulminazzo.it'
+        'developers[0].url'             || 'alex.fulminazzo.it'
+        'developers[0].organization'    || 'Fulminazzo'
+        'developers[0].organizationUrl' || 'fulminazzo.it'
+        'developers[0].roles[0]'        || 'founder'
+        'developers[0].timezone'        || '+1'
+        'developers[0].properties'      || '{contribution=0%}'
+        'developers[1].id'              || 'camu'
+        'developers[1].name'            || 'Camilla'
+        'developers[1].email'           || 'camu@fulminazzo.it'
+        'developers[1].url'             || 'camu.fulminazzo.it'
+        'developers[1].organization'    || 'Fulminazzo'
+        'developers[1].organizationUrl' || 'fulminazzo.it'
+        'developers[1].roles[0]'        || 'co-founder'
+        'developers[1].roles[1]'        || 'accountant'
+        'developers[1].timezone'        || '+1'
+        'developers[1].properties'      || '{contribution=100%}'
     }
 
 }
