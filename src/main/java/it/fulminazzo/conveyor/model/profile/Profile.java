cec43e8445bfeff8c594a8d665a9bf108ead90eb
user <fulminazzo21@gmail.com>
2026-01-18 19:49:43 +0100

    Removed constructor Added SuperBuilder notation and associated builder function

--- src/main/java/it/fulminazzo/conveyor/model/profile/Profile.java
+++ src/main/java/it/fulminazzo/conveyor/model/profile/Profile.java
@@ -1,17 +1,14 @@
 package it.fulminazzo.conveyor.model.profile;
 
 import it.fulminazzo.conveyor.model.MavenModel;
-import it.fulminazzo.conveyor.model.dependency.RawDependency;
 import it.fulminazzo.conveyor.model.profile.activation.Activation;
-import it.fulminazzo.conveyor.model.repository.RawRepository;
 import it.fulminazzo.conveyor.xml.XmlParser;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
-import java.util.Map;
 
 /**
  * Represents a Maven profile.
@@ -19,30 +16,19 @@ import java.util.Map;
 @Getter
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
+@SuperBuilder
 public final class Profile extends MavenModel {
     private final @NotNull String id;
-    private final @NotNull Activation activation;
+    @Builder.Default
+    private final @NotNull Activation activation = Activation.alwaysFalse();
 
     /**
-     * Instantiates a new Profile.
+     * Instantiates a new builder to create a {@link Profile} object.
      *
-     * @param id                   the id
-     * @param activation           the activation
-     * @param properties           the properties
-     * @param repositories         the repositories
-     * @param dependencyManagement the dependency management
-     * @param dependencies         the dependencies
+     * @return the builder
      */
-    Profile(final @NotNull String id,
-            final @NotNull Activation activation,
-            final @NotNull Map<String, String> properties,
-            final @NotNull Collection<RawRepository> repositories,
-            final @NotNull Collection<RawDependency> dependencyManagement,
-            final @NotNull Collection<RawDependency> dependencies
-    ) {
-        super(properties, repositories, dependencyManagement, dependencies);
-        this.id = id;
-        this.activation = activation;
+    public static @NotNull ProfileBuilder<?, ?> builder() {
+        return new ProfileBuilderImpl();
     }
 
     /**
@@ -51,8 +37,8 @@ public final class Profile extends MavenModel {
      * @param parser the XML parser
      * @return the builder
      */
-    public static @NotNull ProfileBuilder builder(final @NotNull XmlParser parser) {
-        return new ProfileBuilder(parser);
+    public static @NotNull XmlProfileBuilder builder(final @NotNull XmlParser parser) {
+        return new XmlProfileBuilder(parser);
     }
 
 }
