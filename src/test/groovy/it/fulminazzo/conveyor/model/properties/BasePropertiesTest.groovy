bc6a75244c151a4d95730b3bdf9d994b93e5eaee
user <fulminazzo21@gmail.com>
2026-01-18 17:48:54 +0100

    Added tests for BaseProperties

---
+++ src/test/groovy/it/fulminazzo/conveyor/model/properties/BasePropertiesTest.groovy
@@ -0,0 +1,26 @@
+package it.fulminazzo.conveyor.model.properties
+
+import spock.lang.Specification
+
+class BasePropertiesTest extends Specification {
+
+    def 'test that BaseProperties correctly applies properties'() {
+        given:
+        def properties = new MockProperties()
+
+        and:
+        properties.putAll([
+                'simple'         : 'Hello',
+                'first'          : 'world!',
+                'second.property': '${first}',
+                'third'          : '${second.property}'
+        ])
+
+        when:
+        def actual = properties.apply('${simple}, ${third} ${unknown}')
+
+        then:
+        actual == 'Hello, world! ${unknown}'
+    }
+
+}
