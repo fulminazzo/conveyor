9b93dc723862b0f89472a83814fbfe0e3b4cda99
user <fulminazzo21@gmail.com>
2026-01-19 18:56:21 +0100

    Created MutableProperties interface

---
+++ src/main/java/it/fulminazzo/conveyor/model/properties/MutableProperties.java
@@ -0,0 +1,32 @@
+package it.fulminazzo.conveyor.model.properties;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+/**
+ * A special type of {@link Properties} that supports adding of properties.
+ */
+public interface MutableProperties extends Properties {
+
+    /**
+     * Adds all the given properties to this object.
+     *
+     * @param properties the raw properties
+     * @return this object (for method chaining)
+     */
+    default @NotNull MutableProperties addAll(final @NotNull Map<String, String> properties) {
+        properties.forEach(this::add);
+        return this;
+    }
+
+    /**
+     * Adds the given property to this object.
+     *
+     * @param key   the key
+     * @param value the value
+     * @return this object (for method chaining)
+     */
+    @NotNull MutableProperties add(final @NotNull String key, final @NotNull String value);
+
+}
