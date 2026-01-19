af237a9f616cc12a080d0880594558f82df9e6d6
user <fulminazzo21@gmail.com>
2026-01-19 18:59:19 +0100

    Created BaseMutableProperties

---
+++ src/main/java/it/fulminazzo/conveyor/model/properties/BaseMutableProperties.java
@@ -0,0 +1,26 @@
+package it.fulminazzo.conveyor.model.properties;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A base implementation of {@link MutableProperties}.
+ */
+final class BaseMutableProperties extends BaseProperties implements MutableProperties {
+    private final @NotNull Map<String, String> map = new HashMap<>();
+
+    @Override
+    public @NotNull MutableProperties add(final @NotNull String key, final @NotNull String value) {
+        this.map.put(key, value);
+        return this;
+    }
+
+    @Override
+    public @Nullable String get(final @NotNull String key) {
+        return this.map.get(key);
+    }
+
+}
