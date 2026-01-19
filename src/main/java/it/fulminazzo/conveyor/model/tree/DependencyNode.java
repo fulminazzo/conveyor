8b8d1cd1d70ce01cd71b58f3cc7ae2643f46d102
user <fulminazzo21@gmail.com>
2026-01-20 00:34:33 +0100

    Added requester field

--- src/main/java/it/fulminazzo/conveyor/model/tree/DependencyNode.java
+++ src/main/java/it/fulminazzo/conveyor/model/tree/DependencyNode.java
@@ -1,25 +1,35 @@
 package it.fulminazzo.conveyor.model.tree;
 
+import it.fulminazzo.conveyor.model.artifact.Artifact;
 import it.fulminazzo.conveyor.model.dependency.Dependency;
 import it.fulminazzo.conveyor.model.dependency.ExclusionsManager;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Represents a node of a {@link Dependency} tree.
  *
- * @param dependency the dependency
- * @param depth      the depth of the node
+ * @param requester         the artifact that requested this dependency
+ * @param dependency        the dependency
+ * @param depth             the depth of the node
+ * @param exclusionsManager the exclusions of the dependency
  */
-public record DependencyNode(@NotNull Dependency dependency, int depth, @NotNull ExclusionsManager exclusionsManager) {
+public record DependencyNode(@Nullable Artifact requester,
+                             @NotNull Dependency dependency,
+                             int depth,
+                             @NotNull ExclusionsManager exclusionsManager) {
 
     /**
      * Instantiates a new Dependency node.
      *
+     * @param requester  the artifact that requested this dependency
      * @param dependency the dependency
      * @param depth      the depth
      */
-    public DependencyNode(final @NotNull Dependency dependency, final int depth) {
-        this(dependency, depth, new ExclusionsManager());
+    public DependencyNode(final @Nullable Artifact requester,
+                          final @NotNull Dependency dependency,
+                          final int depth) {
+        this(requester, dependency, depth, new ExclusionsManager());
     }
 
 }
