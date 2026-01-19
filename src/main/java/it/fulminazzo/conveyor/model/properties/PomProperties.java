e7c4440647e625ee07d4b66006f889bae3a549f9
user <fulminazzo21@gmail.com>
2026-01-19 18:53:23 +0100

    Added getPomDirectory method and parsing of baseDir and baseUri special properties

--- src/main/java/it/fulminazzo/conveyor/model/properties/PomProperties.java
+++ src/main/java/it/fulminazzo/conveyor/model/properties/PomProperties.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,6 +19,7 @@ final class PomProperties extends BaseProperties {
     private static final @NotNull List<String> prefixes = Arrays.asList("project.", "pom.");
 
     private final @NotNull Pom pom;
+    private final @NotNull File workingDir;
 
     @Override
     public @Nullable String get(@NotNull String key) {
@@ -31,10 +33,24 @@ final class PomProperties extends BaseProperties {
             for (String prefix : prefixes)
                 if (key.startsWith(prefix))
                     return null;
-            return this.pom.getProperty(key);
+            if (key.equals("basedir"))
+                return getPomDirectory().getAbsolutePath();
+            else if (key.equals("baseUri"))
+                return getPomDirectory().toURI().toString();
+            else return this.pom.getProperty(key);
         } catch (PropertyAccessorException e) {
             return null;
         }
+    }
+
+    /**
+     * Gets the directory where the {@link #pom} should be stored,
+     * relative to the {@link #workingDir}.
+     *
+     * @return the pom directory
+     */
+    public @NotNull File getPomDirectory() {
+        return new File(this.workingDir, this.pom.getProject().getFullPath("pom")).getParentFile();
     }
 
 }
