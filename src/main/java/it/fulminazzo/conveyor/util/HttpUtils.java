ec9f97368267e80b4b4b87a998b4074ae7b31164
user <fulminazzo21@gmail.com>
2026-01-20 14:21:18 +0100

    Added REDIRECTS_STATUSES field Optimized openConnection function

--- src/main/java/it/fulminazzo/conveyor/util/HttpUtils.java
+++ src/main/java/it/fulminazzo/conveyor/util/HttpUtils.java
@@ -10,8 +10,7 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * A collection of utilities to work with the HTTP protocol
@@ -22,6 +21,15 @@ public final class HttpUtils {
      * The timeout for connection and reading operations.
      */
     static final int CONNECT_READ_TIMEOUT = 10000;
+    /**
+     * All the HTTP status codes considered as redirection.
+     */
+    static final List<Integer> REDIRECTS_STATUSES = Arrays.asList(
+            HttpURLConnection.HTTP_MOVED_PERM,
+            HttpURLConnection.HTTP_MOVED_TEMP,
+            307,
+            308
+    );
 
     /**
      * Opens a new connection to the website for the requested resource,
@@ -40,9 +48,7 @@ public final class HttpUtils {
             connection.setConnectTimeout(CONNECT_READ_TIMEOUT);
             connection.setReadTimeout(CONNECT_READ_TIMEOUT);
             int status = connection.getResponseCode();
-            if (status == HttpURLConnection.HTTP_MOVED_PERM ||
-                    status == HttpURLConnection.HTTP_MOVED_TEMP ||
-                    status == 307 || status == 308) {
+            if (REDIRECTS_STATUSES.contains(status)) {
                 String newUrl = connection.getHeaderField("Location");
                 connection.disconnect();
                 throw new IOException("Redirected to: " + newUrl);
