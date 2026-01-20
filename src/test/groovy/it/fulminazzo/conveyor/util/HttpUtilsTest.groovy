8952061c1998727e19421d038abf787a8a64b39c
user <fulminazzo21@gmail.com>
2026-01-20 15:54:03 +0100

    Added tests for redirect of absolute second branch

--- src/test/groovy/it/fulminazzo/conveyor/util/HttpUtilsTest.groovy
+++ src/test/groovy/it/fulminazzo/conveyor/util/HttpUtilsTest.groovy
@@ -81,15 +81,18 @@ class HttpUtilsTest extends Specification {
         }
 
         where:
-        url                                    | redirect         | resourcePath                 || expectedUrl                 | expectedPath
+        url                                    | redirect                           | resourcePath                 || expectedUrl                 | expectedPath
         // RELATIVE FOUND SUFFIX
-        'https://fulminazzo.it'                | '/it/conveyor'   | '/it/fulminazzo/conveyor'    || 'https://fulminazzo.it/it'  | '/conveyor'
-        'https://fulminazzo.it'                | '/it/conveyor'   | '/it/fulminazzo/theconveyor' || 'https://fulminazzo.it/it/' | 'conveyor'
+        'https://fulminazzo.it'                | '/it/conveyor'                     | '/it/fulminazzo/conveyor'    || 'https://fulminazzo.it/it'  | '/conveyor'
+        'https://fulminazzo.it'                | '/it/conveyor'                     | '/it/fulminazzo/theconveyor' || 'https://fulminazzo.it/it/' | 'conveyor'
         // RELATIVE NOT FOUND SUFFIX
-        'https://fulminazzo.it'                | '/maven/project' | '/it/fulminazzo/conveyor'    || 'https://fulminazzo.it'     | '/maven/project'
-        'https://fulminazzo.it/'               | '/maven/project' | 'it/fulminazzo/conveyor'     || 'https://fulminazzo.it'     | '/maven/project'
-        'https://fulminazzo.it/it/fulminazzo'  | '/maven/project' | '/conveyor'                  || 'https://fulminazzo.it'     | '/maven/project'
-        'https://fulminazzo.it/it/fulminazzo/' | '/maven/project' | 'conveyor'                   || 'https://fulminazzo.it'     | '/maven/project'
+        'https://fulminazzo.it'                | '/maven/project'                   | '/it/fulminazzo/conveyor'    || 'https://fulminazzo.it'     | '/maven/project'
+        'https://fulminazzo.it/'               | '/maven/project'                   | 'it/fulminazzo/conveyor'     || 'https://fulminazzo.it'     | '/maven/project'
+        'https://fulminazzo.it/it/fulminazzo'  | '/maven/project'                   | '/conveyor'                  || 'https://fulminazzo.it'     | '/maven/project'
+        'https://fulminazzo.it/it/fulminazzo/' | '/maven/project'                   | 'conveyor'                   || 'https://fulminazzo.it'     | '/maven/project'
+        // ABSOLUTE NOT FOUND SUFFIX
+        'https://fulminazzo.it'                | 'https://apache.org/maven/project' | '/it/fulminazzo/conveyor'    || 'https://apache.org'        | '/maven/project'
+        'https://fulminazzo.it/'               | 'https://apache.org/maven/project' | 'it/fulminazzo/conveyor'     || 'https://apache.org'        | '/maven/project'
     }
 
     def 'test that extractUrl of #url returns #expected'() {
