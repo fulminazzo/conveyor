8fe9a6339181252ffaeb7efe384facc44d9dda1c
user <fulminazzo21@gmail.com>
2026-01-20 23:13:36 +0100

    Added tests for parseRedhatReleaseFile

--- src/test/groovy/it/fulminazzo/conveyor/model/properties/util/LinuxUtilsTest.groovy
+++ src/test/groovy/it/fulminazzo/conveyor/model/properties/util/LinuxUtilsTest.groovy
@@ -13,8 +13,7 @@ class LinuxUtilsTest extends Specification {
         file.createNewFile()
 
         and:
-        file << """
-NAME="Mock Linux"
+        file << """NAME="Mock Linux"
 PRETTY_NAME="Mock Linux"${id == null ? '' : "\n$LinuxUtils.idPrefix$id"}
 BUILD_ID=rolling${like == null ? '' : "\n$LinuxUtils.idLikePrefix$like"}
 ANSI_COLOR="32;1;24;144;200"${version == null ? '' : "\n$LinuxUtils.versionIdPrefix$version"}
@@ -52,6 +51,34 @@ LOGO=linux
         '"manjaro"' | '"arch"'         | '"6.12"' || new LinuxUtils.Release('manjaro', '6.12', ['arch', 'manjaro'].toSet())
         '"manjaro"' | '"arch manjaro"' | null     || new LinuxUtils.Release('manjaro', null, ['arch', 'manjaro'].toSet())
         '"manjaro"' | '"arch manjaro"' | '"6.12"' || new LinuxUtils.Release('manjaro', '6.12', ['arch', 'manjaro'].toSet())
+    }
+
+    def 'test that parseRedhatReleaseFile with #id and #version returns #expected'() {
+        given:
+        def file = new File(TestUtils.BASE_DIR, 'linux_utils/parse_redhat_release_file')
+        if (file.exists()) file.delete()
+        file.parentFile.mkdirs()
+        file.createNewFile()
+
+        and:
+        file << "${id ?: ''} ${version ?: ''} (Core)"
+
+        when:
+        def actual = LinuxUtils.parseRedhatReleaseFile(file.absolutePath)
+
+        then:
+        actual == expected
+
+        where:
+        id                                 | version    || expected
+        'Rocky Linux release'              | null       || null
+        'Rocky Linux release'              | '9.2'      || null
+        'CentOS'                           | null       || new LinuxUtils.Release('centos', null, LinuxUtils.defaultRedhatVariants.toSet())
+        'CentOS'                           | '7.9.2009' || new LinuxUtils.Release('centos', '7', LinuxUtils.defaultRedhatVariants.toSet())
+        'Fedora'                           | null       || new LinuxUtils.Release('fedora', null, LinuxUtils.defaultRedhatVariants.toSet())
+        'Fedora'                           | '16'       || new LinuxUtils.Release('fedora', '16', LinuxUtils.defaultRedhatVariants.toSet())
+        'Red Hat Enterprise Linux release' | null       || new LinuxUtils.Release('rhel', null, LinuxUtils.defaultRedhatVariants.toSet())
+        'Red Hat Enterprise Linux release' | '8.9'      || new LinuxUtils.Release('rhel', '8', LinuxUtils.defaultRedhatVariants.toSet())
     }
 
 }
