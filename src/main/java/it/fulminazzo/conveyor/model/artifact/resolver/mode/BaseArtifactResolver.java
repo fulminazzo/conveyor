b6f605fd59248559c55141f3fde02565b20e1ac2
user <fulminazzo21@gmail.com>
2026-01-21 01:26:16 +0100

    Updated BaseArtifactResolver to try to fallback to broader classifier in case of too specific one

--- src/main/java/it/fulminazzo/conveyor/model/artifact/resolver/mode/BaseArtifactResolver.java
+++ src/main/java/it/fulminazzo/conveyor/model/artifact/resolver/mode/BaseArtifactResolver.java
@@ -7,6 +7,7 @@ import it.fulminazzo.conveyor.manager.RepositoryManager;
 import it.fulminazzo.conveyor.model.artifact.Artifact;
 import it.fulminazzo.conveyor.model.artifact.resolver.ArtifactResolver;
 import it.fulminazzo.conveyor.model.artifact.resolver.ArtifactResolverException;
+import it.fulminazzo.conveyor.model.properties.Properties;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
@@ -20,18 +21,36 @@ import java.util.Collection;
 @RequiredArgsConstructor
 abstract class BaseArtifactResolver implements ArtifactResolver {
     private final @NotNull RepositoryManager repositoryManager;
+    private final @NotNull Properties osMavenProperties = Properties.newOSMavenProperties();
 
     @Override
     public @NotNull File resolve(final @NotNull Artifact artifact,
                                  final @NotNull String packaging) throws ArtifactResolverException {
+        boolean snapshots = artifact.getVersion().endsWith("-SNAPSHOT");
+        Collection<DownloadSource> sources = snapshots ?
+                this.repositoryManager.getSnapshotsRepositories() :
+                this.repositoryManager.getReleasesRepositories();
         try {
-            final String artifactPath = artifact.getFullPath(packaging);
-            boolean snapshots = artifact.getVersion().endsWith("-SNAPSHOT");
-            return resolve(artifactPath, snapshots ?
-                    this.repositoryManager.getSnapshotsRepositories() :
-                    this.repositoryManager.getReleasesRepositories()
-            );
+            return resolve(artifact.getFullPath(packaging), sources);
         } catch (DownloadException e) {
+            String classifier = artifact.getClassifier();
+            if (classifier != null) {
+                // check if the classifier is too specific, fallback to a broader one.
+                String osClassifier = this.osMavenProperties.get("os.detected.classifier");
+                if (osClassifier != null && classifier.contains(osClassifier)) {
+                    String newClassifier = classifier.substring(0, classifier.indexOf(osClassifier)) + osClassifier;
+                    Artifact newArtifact = Artifact.builder()
+                            .groupId(artifact.getGroupId())
+                            .artifactId(artifact.getArtifactId())
+                            .version(artifact.getVersion())
+                            .classifier(newClassifier)
+                            .build();
+                    try {
+                        return resolve(newArtifact.getFullPath(packaging), sources);
+                    } catch (DownloadException ignored) {
+                    }
+                }
+            }
             throw new ArtifactResolverException(artifact, e);
         }
     }
